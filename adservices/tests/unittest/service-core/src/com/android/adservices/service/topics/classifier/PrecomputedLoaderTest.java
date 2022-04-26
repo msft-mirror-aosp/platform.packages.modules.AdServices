4a7038277cf33167eecf802165fa43fabb4a4932
xufan <xufan@google.com>
2022-04-26 23:37:51 +0000

    Verify that apps classification topics belong to one of the topic in labels file.
    Test: atest AdServicesServiceCoreUnitTests: PrecomputedLoaderTest
    
    No-Typo-Check: Text marked as typo is part of app package name.
    
    Bug: 229760965
    Change-Id: Ifeed133dec70eb063afc425dce0735daa3955854

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/topics/classifier/PrecomputedLoaderTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/topics/classifier/PrecomputedLoaderTest.java
@@ -22,6 +22,8 @@ import androidx.test.core.app.ApplicationProvider;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.collect.ImmutableSet;
+
 import org.junit.Test;
 
 import java.io.IOException;
@@ -34,12 +36,17 @@ import java.util.Map;
  * PrecomputedLoader Test {@link PrecomputedLoader}
  */
 public class PrecomputedLoaderTest {
+    private static final String sLabelsFilePath =
+            "classifier/labels_chrome_topics.txt";
+    private static final String sTestAppFilePath =
+            "classifier/precomputed_test_app_list_chrome_topics.csv";
     private static final Context sContext = ApplicationProvider.getApplicationContext();
-    private static final PrecomputedLoader sPrecomputedLoader = new PrecomputedLoader(sContext);
+    private static final PrecomputedLoader sPrecomputedLoader =
+            new PrecomputedLoader(sContext, sLabelsFilePath, sTestAppFilePath);
 
     @Test
     public void checkLoadedLabels() throws IOException {
-        List<String> labels = sPrecomputedLoader.retrieveLabels();
+        ImmutableSet<String> labels = sPrecomputedLoader.retrieveLabels();
         // Check size of list
         // The labels.txt contains 350 topics
         assertThat(labels.size()).isEqualTo(349);
@@ -56,8 +63,8 @@ public class PrecomputedLoaderTest {
     public void checkLoadedAppTopics() throws IOException {
         Map<String, List<String>> appTopic = sPrecomputedLoader.retrieveAppClassificationTopics();
         // Check size of map
-        // The app topics file contains 1000 apps + 11 sample apps.
-        assertThat(appTopic.size()).isEqualTo(1011);
+        // The app topics file contains 1000 apps + 11 sample apps + 2 test valid topics' apps.
+        assertThat(appTopic.size()).isEqualTo(1013);
 
         // Check whatsApp, chrome and a sample app topics in map
         List<String> whatsAppTopics =
@@ -96,5 +103,23 @@ public class PrecomputedLoaderTest {
                     "com.example.adservices.samples.topics.sampleapp" + appIndex)).size())
                     .isEqualTo(5);
         }
+
+        // Verify that the topics from the file are valid:
+        // the valid topic is one of the topic in the labels file.
+        // The invalid topics will not be loaded in the app topics map.
+        List<String> validTestApp1Topics =
+                Arrays.asList(
+                        "/Internet & Telecom/Text & Instant Messaging",
+                        "/News",
+                        "/People & Society");
+        assertThat(appTopic.get("com.example.adservices.valid.topics.testapp1"))
+                .isEqualTo(validTestApp1Topics);
+
+        List<String> validTestApp2Topics =
+                Arrays.asList(
+                        "/Arts & Entertainment/Online Video",
+                        "/Computers & Electronics/Consumer Electronics");
+        assertThat(appTopic.get("com.example.adservices.valid.topics.testapp2"))
+                .isEqualTo(validTestApp2Topics);
     }
 }
