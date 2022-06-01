425992f29dfe81fd9103bfd49dd00dc5f1188144
Han <hanlixy@google.com>
2022-06-01 17:48:40 -0400

    Add Taxonomy version and model versions for tables that includes TopicId
    Bug: 234222849
    Test: atest AdServicesServiceCoreUnitTests --iterations 2
    Change-Id: I3d79b060c4405518524ce9aa17c3d11623e76d4a

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/topics/TopicsServiceImplTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/topics/TopicsServiceImplTest.java
@@ -136,10 +136,9 @@ public class TopicsServiceImplTest {
         // persist returned topics into DB
         for (int numEpoch = 1; numEpoch <= numberOfLookBackEpochs; numEpoch++) {
             Topic currentTopic = topics[numberOfLookBackEpochs - numEpoch];
-            Map<Pair<String, String>, Integer> returnedAppSdkTopicsMap = new HashMap<>();
-            returnedAppSdkTopicsMap.put(appSdkKey, currentTopic.getTopic());
-            mTopicsDao.persistReturnedAppTopicsMap(numEpoch, currentTopic.getTaxonomyVersion(),
-                    currentTopic.getModelVersion(), returnedAppSdkTopicsMap);
+            Map<Pair<String, String>, Topic> returnedAppSdkTopicsMap = new HashMap<>();
+            returnedAppSdkTopicsMap.put(appSdkKey, currentTopic);
+            mTopicsDao.persistReturnedAppTopicsMap(numEpoch, returnedAppSdkTopicsMap);
         }
 
         when(mMockEpochManager.getCurrentEpochId()).thenReturn(currentEpochId);
