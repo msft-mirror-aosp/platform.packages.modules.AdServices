577ee2472df2d13dc667fe17ab4327a9176a308f
Yuxuan Chen <yukichen@google.com>
2022-06-13 21:45:43 +0000

    Update Topics API to return a list of Topics instead of 3 parallel lists
    Bug: 235640984
    
    Test: atest
    
    Change-Id: I5c58e8c14501c7946e9799ca4480566ca6a70c88

--- adservices/framework/java/android/adservices/topics/TopicsManager.java
+++ adservices/framework/java/android/adservices/topics/TopicsManager.java
@@ -34,6 +34,8 @@ import com.android.adservices.ServiceBinder;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Executor;
 
@@ -170,12 +172,7 @@ public class TopicsManager {
                                         if (resultParcel.isSuccess()) {
                                             callback.onResult(
                                                     new GetTopicsResponse.Builder()
-                                                            .setTaxonomyVersions(
-                                                                    resultParcel
-                                                                            .getTaxonomyVersions())
-                                                            .setModelVersions(
-                                                                    resultParcel.getModelVersions())
-                                                            .setTopics(resultParcel.getTopics())
+                                                            .setTopics(getTopicList(resultParcel))
                                                             .build());
                                         } else {
                                             // TODO: Errors should be returned in onFailure method.
@@ -203,6 +200,24 @@ public class TopicsManager {
             LogUtil.e("RemoteException", e);
             callback.onError(new GetTopicsException(RESULT_INTERNAL_ERROR, "Internal Error!"));
         }
+    }
+
+    private List<Topic> getTopicList(GetTopicsResult resultParcel) {
+        List<Long> taxonomyVersionsList = resultParcel.getTaxonomyVersions();
+        List<Long> modelVersionsList = resultParcel.getModelVersions();
+        List<Integer> topicsCodeList = resultParcel.getTopics();
+        List<Topic> topicList = new ArrayList<>();
+        int size = taxonomyVersionsList.size();
+        for (int i = 0; i < size; i++) {
+            Topic topic =
+                    new Topic(
+                            taxonomyVersionsList.get(i),
+                            modelVersionsList.get(i),
+                            topicsCodeList.get(i));
+            topicList.add(topic);
+        }
+
+        return topicList;
     }
 
     /**
