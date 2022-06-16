49023ebe949feb392c04da7981646de604b671b6
Abhimanyu <abmehta@google.com>
2022-06-16 23:49:33 +0000

    Parse web source and trigger registration request and headers
    Test: TriggerFetcherTest, SourceFetcherTest, MeasurementImplTest,
    MeasurementServiceImplTest
    
    Bug: b/237501084
    
    Change-Id: Ib86179ec2a33b804a88f01f5efbb046ced89f3bf

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/measurement/MeasurementServiceImplTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/measurement/MeasurementServiceImplTest.java
@@ -29,10 +29,16 @@ import android.adservices.measurement.IMeasurementApiStatusCallback;
 import android.adservices.measurement.IMeasurementCallback;
 import android.adservices.measurement.MeasurementApiUtil;
 import android.adservices.measurement.MeasurementErrorResponse;
+import android.adservices.measurement.MeasurementManager;
 import android.adservices.measurement.RegistrationRequest;
+import android.adservices.measurement.WebSourceParams;
+import android.adservices.measurement.WebSourceRegistrationRequest;
+import android.adservices.measurement.WebSourceRegistrationRequestInternal;
+import android.adservices.measurement.WebTriggerParams;
+import android.adservices.measurement.WebTriggerRegistrationRequest;
+import android.adservices.measurement.WebTriggerRegistrationRequestInternal;
 import android.content.Context;
 import android.net.Uri;
-import android.os.RemoteException;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.SmallTest;
@@ -42,6 +48,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -50,8 +57,21 @@ import java.util.concurrent.TimeUnit;
 @SmallTest
 public final class MeasurementServiceImplTest {
 
+    private static final Uri REGISTRATION_URI = Uri.parse("https://registration-uri.com");
+    private static final Uri WEB_DESTINATION = Uri.parse("https://web-destination-uri.com");
+    private static final Uri OS_DESTINATION = Uri.parse("https://os-destination-uri.com");
     private static final Context sContext = ApplicationProvider.getApplicationContext();
     private static final int TIMEOUT = 5_000;
+    private static final WebSourceParams SOURCE_REGISTRATION =
+            new WebSourceParams.Builder()
+                    .setRegistrationUri(REGISTRATION_URI)
+                    .setAllowDebugKey(true)
+                    .build();
+    private static final WebTriggerParams TRIGGER_REGISTRATION =
+            new WebTriggerParams.Builder()
+                    .setRegistrationUri(REGISTRATION_URI)
+                    .setAllowDebugKey(true)
+                    .build();
     private MeasurementImpl mMockMeasurementImpl;
 
     @Before
@@ -59,6 +79,12 @@ public final class MeasurementServiceImplTest {
         mMockMeasurementImpl = Mockito.mock(MeasurementImpl.class);
         when(mMockMeasurementImpl.register(any(RegistrationRequest.class), anyLong()))
                 .thenReturn(RESULT_OK);
+        when(mMockMeasurementImpl.registerWebSource(
+                        any(WebSourceRegistrationRequestInternal.class), anyLong()))
+                .thenReturn(MeasurementManager.RESULT_OK);
+        when(mMockMeasurementImpl.registerWebSource(
+                        any(WebSourceRegistrationRequestInternal.class), anyLong()))
+                .thenReturn(MeasurementManager.RESULT_OK);
     }
 
     @Test
@@ -71,7 +97,7 @@ public final class MeasurementServiceImplTest {
                         getDefaultRegistrationRequest(),
                         new IMeasurementCallback.Stub() {
                             @Override
-                            public void onResult() throws RemoteException {
+                            public void onResult() {
                                 list.add(RESULT_OK);
                                 countDownLatch.countDown();
                             }
@@ -85,13 +111,13 @@ public final class MeasurementServiceImplTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void testRegister_invalidRequest() throws Exception {
+    public void testRegister_invalidRequest() {
         new MeasurementServiceImpl(mMockMeasurementImpl)
                 .register(
                         null,
                         new IMeasurementCallback.Stub() {
                             @Override
-                            public void onResult() throws RemoteException {}
+                            public void onResult() {}
 
                             @Override
                             public void onFailure(MeasurementErrorResponse responseParcel) {}
@@ -99,7 +125,7 @@ public final class MeasurementServiceImplTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void testRegister_invalidCallback() throws Exception {
+    public void testRegister_invalidCallback() {
         new MeasurementServiceImpl(mMockMeasurementImpl).register(
                 getDefaultRegistrationRequest(),
                 null
@@ -116,7 +142,7 @@ public final class MeasurementServiceImplTest {
                         getDefaultDeletionRequest(),
                         new IMeasurementCallback.Stub() {
                             @Override
-                            public void onResult() throws RemoteException {
+                            public void onResult() {
                                 list.add(RESULT_OK);
                                 countDownLatch.countDown();
                             }
@@ -130,13 +156,13 @@ public final class MeasurementServiceImplTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void testDeleteRegistrations_invalidRequest() throws Exception {
+    public void testDeleteRegistrations_invalidRequest() {
         new MeasurementServiceImpl(mMockMeasurementImpl)
                 .deleteRegistrations(
                         null,
                         new IMeasurementCallback.Stub() {
                             @Override
-                            public void onResult() throws RemoteException {}
+                            public void onResult() {}
 
                             @Override
                             public void onFailure(MeasurementErrorResponse responseParcel) {}
@@ -144,7 +170,7 @@ public final class MeasurementServiceImplTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void testDeleteRegistrations_invalidCallback() throws Exception {
+    public void testDeleteRegistrations_invalidCallback() {
         new MeasurementServiceImpl(mMockMeasurementImpl).deleteRegistrations(
                 getDefaultDeletionRequest(),
                 null
@@ -157,23 +183,113 @@ public final class MeasurementServiceImplTest {
         CountDownLatch countDownLatch = new CountDownLatch(1);
         final List<Integer> list = new ArrayList<>();
 
-        new MeasurementServiceImpl(measurementImpl).getMeasurementApiStatus(
-                new IMeasurementApiStatusCallback.Stub() {
-                    @Override
-                    public void onResult(int result) throws RemoteException {
-                        list.add(result);
-                        countDownLatch.countDown();
-                    }
-                }
-        );
+        new MeasurementServiceImpl(measurementImpl)
+                .getMeasurementApiStatus(
+                        new IMeasurementApiStatusCallback.Stub() {
+                            @Override
+                            public void onResult(int result) {
+                                list.add(result);
+                                countDownLatch.countDown();
+                            }
+                        });
 
         assertThat(countDownLatch.await(TIMEOUT, TimeUnit.MILLISECONDS)).isTrue();
         assertThat(list.get(0)).isEqualTo(MeasurementApiUtil.MEASUREMENT_API_STATE_ENABLED);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testGetMeasurementApiStatus_invalidCallback() throws Exception {
+    public void testGetMeasurementApiStatus_invalidCallback() {
         new MeasurementServiceImpl(mMockMeasurementImpl).getMeasurementApiStatus(null);
+    }
+
+    @Test
+    public void registerWebSource_success() throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        final List<Integer> list = new ArrayList<>();
+
+        new MeasurementServiceImpl(mMockMeasurementImpl)
+                .registerWebSource(
+                        createWebSourceRegistrationRequest(),
+                        new IMeasurementCallback.Stub() {
+                            @Override
+                            public void onResult() {
+                                list.add(RESULT_OK);
+                                countDownLatch.countDown();
+                            }
+
+                            @Override
+                            public void onFailure(
+                                    MeasurementErrorResponse measurementErrorResponse) {}
+                        });
+
+        assertThat(countDownLatch.await(TIMEOUT, TimeUnit.MILLISECONDS)).isTrue();
+        assertThat(list.get(0)).isEqualTo(MeasurementManager.RESULT_OK);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void registerWebSource_invalidRequest() {
+        new MeasurementServiceImpl(mMockMeasurementImpl)
+                .registerWebSource(
+                        null,
+                        new IMeasurementCallback.Stub() {
+                            @Override
+                            public void onResult() {}
+
+                            @Override
+                            public void onFailure(
+                                    MeasurementErrorResponse measurementErrorResponse) {}
+                        });
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void registerWebSource_invalidCallback() {
+        new MeasurementServiceImpl(mMockMeasurementImpl)
+                .registerWebSource(createWebSourceRegistrationRequest(), null);
+    }
+
+    @Test
+    public void registerWebTrigger_success() throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        final List<Integer> list = new ArrayList<>();
+
+        new MeasurementServiceImpl(mMockMeasurementImpl)
+                .registerWebTrigger(
+                        createWebTriggerRegistrationRequest(),
+                        new IMeasurementCallback.Stub() {
+                            @Override
+                            public void onResult() {
+                                list.add(RESULT_OK);
+                                countDownLatch.countDown();
+                            }
+
+                            @Override
+                            public void onFailure(
+                                    MeasurementErrorResponse measurementErrorResponse) {}
+                        });
+
+        assertThat(countDownLatch.await(TIMEOUT, TimeUnit.MILLISECONDS)).isTrue();
+        assertThat(list.get(0)).isEqualTo(MeasurementManager.RESULT_OK);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void registerWebTrigger_invalidRequest() {
+        new MeasurementServiceImpl(mMockMeasurementImpl)
+                .registerWebSource(
+                        null,
+                        new IMeasurementCallback.Stub() {
+                            @Override
+                            public void onResult() {}
+
+                            @Override
+                            public void onFailure(
+                                    MeasurementErrorResponse measurementErrorResponse) {}
+                        });
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void registerWebTrigger_invalidCallback() {
+        new MeasurementServiceImpl(mMockMeasurementImpl)
+                .registerWebTrigger(createWebTriggerRegistrationRequest(), null);
     }
 
     private RegistrationRequest getDefaultRegistrationRequest() {
@@ -182,6 +298,32 @@ public final class MeasurementServiceImplTest {
                 .setRegistrationUri(Uri.parse("https://registration-uri.com"))
                 .setTopOriginUri(Uri.parse("android-app//com.example"))
                 .setRegistrationType(RegistrationRequest.REGISTER_SOURCE)
+                .build();
+    }
+
+    private WebSourceRegistrationRequestInternal createWebSourceRegistrationRequest() {
+        WebSourceRegistrationRequest sourceRegistrationRequest =
+                new WebSourceRegistrationRequest.Builder()
+                        .setSourceParams(Collections.singletonList(SOURCE_REGISTRATION))
+                        .setWebDestination(WEB_DESTINATION)
+                        .setOsDestination(OS_DESTINATION)
+                        .setTopOriginUri(Uri.parse("android-app//com.example"))
+                        .build();
+        return new WebSourceRegistrationRequestInternal.Builder()
+                .setSourceRegistrationRequest(sourceRegistrationRequest)
+                .setAttributionSource(sContext.getAttributionSource())
+                .build();
+    }
+
+    private WebTriggerRegistrationRequestInternal createWebTriggerRegistrationRequest() {
+        WebTriggerRegistrationRequest webTriggerRegistrationRequest =
+                new WebTriggerRegistrationRequest.Builder()
+                        .setTriggerParams(Collections.singletonList(TRIGGER_REGISTRATION))
+                        .setDestination(Uri.parse("android-app//com.example"))
+                        .build();
+        return new WebTriggerRegistrationRequestInternal.Builder()
+                .setTriggerRegistrationRequest(webTriggerRegistrationRequest)
+                .setAttributionSource(sContext.getAttributionSource())
                 .build();
     }
 
