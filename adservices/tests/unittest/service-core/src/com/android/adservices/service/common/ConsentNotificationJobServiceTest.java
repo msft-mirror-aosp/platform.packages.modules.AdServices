b8e038f25858548587d0bf4ac390ef212073baa5
chenjiahui <chenjiahui@google.com>
2022-07-27 05:23:57 +0000

    update adservice scheduler to schedule notification job
    This is a change that moved notification scheduler to service side and
    add debug mode and consumer to trigger notification, when we receive
    enable status from privacy sandbox.
    
    Bug: 238356195
    Test: atest AdservicesSyncUtilTest ConsentNotificationJobServiceTest
    
    Change-Id: Iedc0d768ada444216751c1df1c69263d8817705c

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/common/ConsentNotificationJobServiceTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/common/ConsentNotificationJobServiceTest.java
@@ -14,25 +14,29 @@
  * limitations under the License.
  */
 
-package com.android.adservices.ui.notifications;
+package com.android.adservices.service.common;
 
-import static com.android.adservices.ui.notifications.ConsentNotificationJobService.MILLISECONDS_IN_THE_DAY;
+import static com.android.adservices.service.common.ConsentNotificationJobService.MILLISECONDS_IN_THE_DAY;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.any;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.doAnswer;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.doNothing;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.doReturn;
-import static com.android.dx.mockito.inline.extended.ExtendedMockito.staticMockMarker;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.verify;
-import static com.android.dx.mockito.inline.extended.ExtendedMockito.verifyNoMoreInteractions;
 
 import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import android.app.job.JobParameters;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.PersistableBundle;
 
 import androidx.test.core.app.ApplicationProvider;
 
+import com.android.adservices.service.Flags;
 import com.android.adservices.service.FlagsFactory;
 import com.android.adservices.service.consent.ConsentManager;
 import com.android.dx.mockito.inline.extended.ExtendedMockito;
@@ -61,6 +65,9 @@ public class ConsentNotificationJobServiceTest {
 
     @Mock JobParameters mMockJobParameters;
     @Mock PackageManager mPackageManager;
+    @Mock AdServicesSyncUtil mAdservicesSyncUtil;
+    @Mock PersistableBundle mPersistableBundle;
+    @Mock Flags mFlags;
     private MockitoSession mStaticMockSession = null;
 
     /** Initialize static spies. */
@@ -73,7 +80,7 @@ public class ConsentNotificationJobServiceTest {
                 ExtendedMockito.mockitoSession()
                         .spyStatic(FlagsFactory.class)
                         .spyStatic(ConsentManager.class)
-                        .spyStatic(ConsentNotificationTrigger.class)
+                        .spyStatic(AdServicesSyncUtil.class)
                         .spyStatic(ConsentNotificationJobService.class)
                         .strictness(Strictness.WARN)
                         .initMocks(this)
@@ -90,7 +97,7 @@ public class ConsentNotificationJobServiceTest {
 
     /** Test successful onStart method execution when notification was not yet displayed. */
     @Test
-    public void testOnStartJobNotificationNotDisplayed() throws InterruptedException {
+    public void testOnStartJobAsyncUtilExecute() throws InterruptedException {
         CountDownLatch jobFinishedCountDown = new CountDownLatch(1);
 
         doReturn(mPackageManager).when(mConsentNotificationJobService).getPackageManager();
@@ -101,55 +108,24 @@ public class ConsentNotificationJobServiceTest {
         mConsentNotificationJobService.setConsentManager(mConsentManager);
         doReturn(mConsentManager).when(() -> ConsentManager.getInstance(any(Context.class)));
         doReturn(true).when(() -> ConsentNotificationJobService.isEuDevice(any(Context.class)));
-        doNothing()
-                .when(
-                        () ->
-                                ConsentNotificationTrigger.showConsentNotification(
-                                        any(Context.class), any(Boolean.class)));
+        when(mMockJobParameters.getExtras()).thenReturn(mPersistableBundle);
+        when(mPersistableBundle.getBoolean(anyString(), anyBoolean())).thenReturn(true);
+        doReturn(mAdservicesSyncUtil).when(() -> AdServicesSyncUtil.getInstance());
         doAnswer(unusedInvocation -> {
             jobFinishedCountDown.countDown();
             return null;
         }).when(mConsentNotificationJobService).jobFinished(mMockJobParameters, false);
+
+        doNothing().when(mAdservicesSyncUtil).execute(any(Context.class), any(Boolean.class));
+        doReturn(mFlags).when(() -> FlagsFactory.getFlags());
+        when(mFlags.getConsentNotificationDebugMode()).thenReturn(false);
 
         mConsentNotificationJobService.onStartJob(mMockJobParameters);
         jobFinishedCountDown.await();
 
         verify(mConsentManager).wasNotificationDisplayed(any());
+        verify(mAdservicesSyncUtil).execute(any(Context.class), any(Boolean.class));
         verify(mConsentNotificationJobService).jobFinished(mMockJobParameters, false);
-        verify(
-                () ->
-                        ConsentNotificationTrigger.showConsentNotification(
-                                any(Context.class), any(Boolean.class)));
-    }
-
-    /** Test successful onStart method execution when notification was already displayed. */
-    @Test
-    public void testOnStartJobNotificationDisplayed() throws InterruptedException {
-        CountDownLatch jobFinishedCountDown = new CountDownLatch(1);
-
-        doReturn(mPackageManager).when(mConsentNotificationJobService).getPackageManager();
-        doReturn(Boolean.TRUE)
-                .when(mConsentManager)
-                .wasNotificationDisplayed(any(PackageManager.class));
-        doNothing().when(mConsentManager).recordNotificationDisplayed(any());
-        mConsentNotificationJobService.setConsentManager(mConsentManager);
-        doReturn(mConsentManager).when(() -> ConsentManager.getInstance(any(Context.class)));
-        doReturn(true).when(() -> ConsentNotificationJobService.isEuDevice(any(Context.class)));
-        doNothing()
-                .when(
-                        () ->
-                                ConsentNotificationTrigger.showConsentNotification(
-                                        any(Context.class), any(Boolean.class)));
-        doAnswer(unusedInvocation -> {
-            jobFinishedCountDown.countDown();
-            return null;
-        }).when(mConsentNotificationJobService).jobFinished(mMockJobParameters, false);
-
-        mConsentNotificationJobService.onStartJob(mMockJobParameters);
-        jobFinishedCountDown.await();
-
-        verify(mConsentNotificationJobService).jobFinished(mMockJobParameters, false);
-        verifyNoMoreInteractions(staticMockMarker(ConsentNotificationTrigger.class));
     }
 
     /** Test successful onStop method execution. */
@@ -236,5 +212,17 @@ public class ConsentNotificationJobServiceTest {
                         midnight
                                 + FlagsFactory.getFlagsForTest()
                                         .getConsentNotificationIntervalEndMs());
+    }
+
+    @Test
+    public void testDelaysWhenDebugModeOn() {
+        doReturn(mFlags).when(() -> FlagsFactory.getFlags());
+        when(mFlags.getConsentNotificationDebugMode()).thenReturn(true);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(0);
+        long initialDelay = ConsentNotificationJobService.calculateInitialDelay(calendar);
+        long deadline = ConsentNotificationJobService.calculateDeadline(calendar);
+        assertThat(initialDelay).isEqualTo(0L);
+        assertThat(deadline).isEqualTo(0L);
     }
 }
