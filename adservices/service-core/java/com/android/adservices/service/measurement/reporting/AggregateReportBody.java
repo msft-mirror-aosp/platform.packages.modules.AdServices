222da0ebe2292adbd9162945eba6c98e52391761
Nick Chalko <nchalko@google.com>
2022-09-08 20:16:00 +0000

    Revert "[automerge] debug api permission 2p: 5fca5b87a0"
    Revert "debug api permission"
    
    Revert submission 19866926-presubmit-am-e3a6270261fc4c46b7cc7cdbf07efa7d
    
    Bug: 245750402
    
    Reason for revert: DroidMonitor: Potential culprit for Bug 245750402 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    Reverted Changes:
    I9e6079aa7:debug api permission
    Ic0c239cf1:[automerge] debug api permission 2p: 5fca5b87a0
    
    Change-Id: I598d16e5e62688eef4052520c99ede4967ec7a11

--- adservices/service-core/java/com/android/adservices/service/measurement/reporting/AggregateReportBody.java
+++ adservices/service-core/java/com/android/adservices/service/measurement/reporting/AggregateReportBody.java
@@ -93,12 +93,10 @@ public class AggregateReportBody {
                 aggregationServicePayloadsToJson(sharedInfo, key));
 
         if (mSourceDebugKey != null) {
-            aggregateBodyJson.put(
-                    PayloadBodyKeys.SOURCE_DEBUG_KEY, Long.toUnsignedString(mSourceDebugKey));
+            aggregateBodyJson.put(PayloadBodyKeys.SOURCE_DEBUG_KEY, mSourceDebugKey);
         }
         if (mTriggerDebugKey != null) {
-            aggregateBodyJson.put(
-                    PayloadBodyKeys.TRIGGER_DEBUG_KEY, Long.toUnsignedString(mTriggerDebugKey));
+            aggregateBodyJson.put(PayloadBodyKeys.TRIGGER_DEBUG_KEY, mTriggerDebugKey);
         }
 
         return aggregateBodyJson;
