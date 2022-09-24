a618101a54fc99bee5ee5a82ba07769b6e740e3a
fayey <fayey@google.com>
2022-09-24 06:54:37 +0000

    Add data type classes for the fledge internal process for atoms: 1.RunAdSelectionProcessReported 2.RunAdBiddingProcessReported 3.RunAdScoringProcessReported 4.RunAdBiddingPerCAProcessReported 5.BackgroundFetchProcessReported 6.UpdateCustomAudienceProcessReported
    Test: atest
    
    bug: 221862367
    bug: 221861861
    Change-Id: Iba35205b21aee413b8b3329079c633c2ab40bf8d

---
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/stats/BackgroundFetchProcessReportedStatsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.service.stats;
+
+import static com.android.adservices.service.stats.AdServicesStatsLog.BACKGROUND_FETCH_PROCESS_REPORTED__RESULT_CODE__BACKGROUND_FETCH_SUCCESS;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/** Unit tests for {@link BackgroundFetchProcessReportedStats}. */
+public class BackgroundFetchProcessReportedStatsTest {
+    private static final int LATENCY_IN_MILLIS = 10;
+    private static final int NUM_OF_ELIGIBLE_TO_UPDATE_CAS = 5;
+    private static final int RESULT_CODE =
+            BACKGROUND_FETCH_PROCESS_REPORTED__RESULT_CODE__BACKGROUND_FETCH_SUCCESS;
+
+    @Test
+    public void testBuilderCreateSuccess() {
+        BackgroundFetchProcessReportedStats stats =
+                BackgroundFetchProcessReportedStats.builder()
+                        .setLatencyInMillis(LATENCY_IN_MILLIS)
+                        .setNumOfEligibleToUpdateCas(NUM_OF_ELIGIBLE_TO_UPDATE_CAS)
+                        .setResultCode(RESULT_CODE)
+                        .build();
+        assertEquals(LATENCY_IN_MILLIS, stats.getLatencyInMillis());
+        assertEquals(NUM_OF_ELIGIBLE_TO_UPDATE_CAS, stats.getNumOfEligibleToUpdateCas());
+        assertEquals(RESULT_CODE, stats.getResultCode());
+    }
+}
---
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/stats/RunAdSelectionProcessReportedStatsTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.service.stats;
+
+import static com.android.adservices.service.stats.AdServicesStatsLog.RUN_AD_BIDDING_PROCESS_REPORTED__RUN_AD_BIDDING_RESULT_CODE__RUN_AD_SELECTION_STATUS_SUCCESS;
+import static com.android.adservices.service.stats.AdServicesStatsLog.RUN_AD_SELECTION_PROCESS_REPORTED__PERSIST_AD_SELECTION_RESULT_CODE__RUN_AD_SELECTION_STATUS_SUCCESS;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/** Unit tests for {@link RunAdSelectionProcessReportedStats}. */
+public class RunAdSelectionProcessReportedStatsTest {
+    private static final boolean IS_RMKT_ADS_WON = true;
+    private static final int AD_SELECTION_ENTRY_SIZE_IN_BYTES = 100;
+    private static final int PERSIST_AD_SELECTION_LATENCY_IN_MILLIS = 10;
+    private static final int RUN_AD_SELECTION_LATENCY_IN_MILLIS = 10;
+    private static final int PERSIST_AD_SELECTION_RESULT_CODE =
+            RUN_AD_SELECTION_PROCESS_REPORTED__PERSIST_AD_SELECTION_RESULT_CODE__RUN_AD_SELECTION_STATUS_SUCCESS;
+    private static final int RUN_AD_SELECTION_RESULT_CODE =
+            RUN_AD_BIDDING_PROCESS_REPORTED__RUN_AD_BIDDING_RESULT_CODE__RUN_AD_SELECTION_STATUS_SUCCESS;
+
+    @Test
+    public void testBuilderCreateSuccess() {
+        RunAdSelectionProcessReportedStats stats =
+                RunAdSelectionProcessReportedStats.builder()
+                        .setIsRemarketingAdsWon(IS_RMKT_ADS_WON)
+                        .setAdSelectionEntrySizeInBytes(AD_SELECTION_ENTRY_SIZE_IN_BYTES)
+                        .setPersistAdSelectionLatencyInMillis(
+                                PERSIST_AD_SELECTION_LATENCY_IN_MILLIS)
+                        .setPersistAdSelectionResultCode(PERSIST_AD_SELECTION_RESULT_CODE)
+                        .setRunAdSelectionLatencyInMillis(RUN_AD_SELECTION_LATENCY_IN_MILLIS)
+                        .setRunAdSelectionResultCode(RUN_AD_SELECTION_RESULT_CODE)
+                        .build();
+        assertEquals(IS_RMKT_ADS_WON, stats.getIsRemarketingAdsWon());
+        assertEquals(AD_SELECTION_ENTRY_SIZE_IN_BYTES, stats.getAdSelectionEntrySizeInBytes());
+        assertEquals(
+                PERSIST_AD_SELECTION_LATENCY_IN_MILLIS,
+                stats.getPersistAdSelectionLatencyInMillis());
+        assertEquals(PERSIST_AD_SELECTION_RESULT_CODE, stats.getPersistAdSelectionResultCode());
+        assertEquals(RUN_AD_SELECTION_LATENCY_IN_MILLIS, stats.getRunAdSelectionLatencyInMillis());
+        assertEquals(RUN_AD_SELECTION_RESULT_CODE, stats.getRunAdSelectionResultCode());
+    }
+}
