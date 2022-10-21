6492bc5033fe01450c19f4ae39c81c0937a6b133
Sanjana Sunil <sanjanasunil@google.com>
2022-10-21 10:13:18 +0000

    Check for multiuser support in test
    Verify that a secondary user can be created before attempting to do so.
    
    Bug: 254475062
    Test: atest SdkSandboxDataIsolationHostTest
    Change-Id: I8e5cf6a95cb7ca961be857673f896d236c48e696

--- sdksandbox/tests/cts/hostside/src/com/android/sdksandbox/cts/host/SdkSandboxDataIsolationHostTest.java
+++ sdksandbox/tests/cts/hostside/src/com/android/sdksandbox/cts/host/SdkSandboxDataIsolationHostTest.java
@@ -116,6 +116,8 @@ public class SdkSandboxDataIsolationHostTest extends BaseHostJUnit4Test {
      */
     @Test
     public void testSdkSandboxDataIsolation_CannotVerifyOtherUserAppExistence() throws Exception {
+        assumeTrue(getDevice().isMultiUserSupported());
+
         installPackage(APP_APK);
 
         int userId = mUserUtils.createAndStartSecondaryUser();
