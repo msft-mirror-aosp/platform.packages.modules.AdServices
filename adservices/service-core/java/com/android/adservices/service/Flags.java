57995035fdb98563ad23a95f0812c883dcd724c1
Manali Bhutiyani <manalib@google.com>
2022-11-01 05:54:00 +0000

    [appsetid] allowlist appsetid sandbox test
    Bug: 255652368
    Test: atest
    
    Change-Id: I8249e8f4d6c548f0c6b2ddc2571e6fbd5050f0e2

--- adservices/service-core/java/com/android/adservices/service/Flags.java
+++ adservices/service-core/java/com/android/adservices/service/Flags.java
@@ -1082,6 +1082,7 @@ public interface Flags extends Dumpable {
                     + "com.android.adservices.tests.appsetid,"
                     + "com.android.sdksandboxclient,"
                     + "com.android.tests.sandbox.adid,"
+                    + "com.android.tests.sandbox.appsetid,"
                     + "com.android.tests.sandbox.fledge,"
                     + "com.android.tests.sandbox.measurement,"
                     + "com.example.adservices.samples.adid.app,"
