cb04f65ebea71478d09323ab0b38f8f7e56558ba
John <johnyy@google.com>
2022-11-09 01:17:36 +0000

    Adding AdSelectionFromOutcomesInputValidator
    Bug: 258020359
    Test: atest com.android.adservices.service.adselection.AdSelectionServiceImplTest \
                com.android.adservices.service.adselection.AdSelectionFromOutcomesInputValidatorTest
    Change-Id: I8453366c699b74d1f60a3ebf76a8075290cb7aac

--- adservices/tests/unittest/fixtures/java/android/adservices/adselection/AdSelectionOutcomeFixture.java
+++ adservices/tests/unittest/fixtures/java/android/adservices/adselection/AdSelectionOutcomeFixture.java
@@ -18,6 +18,14 @@ package android.adservices.adselection;
 
 import android.net.Uri;
 
+import androidx.annotation.NonNull;
+
+import com.android.adservices.data.adselection.AdSelectionEntryDao;
+import com.android.adservices.data.adselection.CustomAudienceSignals;
+import com.android.adservices.data.adselection.DBAdSelection;
+
+import java.time.Instant;
+
 public class AdSelectionOutcomeFixture {
     public static final long SAMPLE_AD_SELECTION_ID1 = 12345;
     public static final long SAMPLE_AD_SELECTION_ID2 = 123456;
@@ -31,17 +39,52 @@ public class AdSelectionOutcomeFixture {
                 .build();
     }
 
-    public static AdSelectionOutcome anAdSelectionOutcome(long adSelectionId) {
+    public static AdSelectionOutcome anAdSelectionOutcome(final long adSelectionId) {
         return new AdSelectionOutcome.Builder()
                 .setAdSelectionId(adSelectionId)
                 .setRenderUri(SAMPLE_RENDER_URI1)
                 .build();
     }
 
-    public static AdSelectionOutcome anAdSelectionOutcome(Uri renderUri) {
+    public static AdSelectionOutcome anAdSelectionOutcome(final Uri renderUri) {
         return new AdSelectionOutcome.Builder()
                 .setAdSelectionId(SAMPLE_AD_SELECTION_ID1)
                 .setRenderUri(renderUri)
                 .build();
+    }
+
+    public static AdSelectionOutcome anAdSelectionOutcome(
+            final long adSelectionId, final Uri renderUri) {
+        return new AdSelectionOutcome.Builder()
+                .setAdSelectionId(adSelectionId)
+                .setRenderUri(renderUri)
+                .build();
+    }
+
+    public static AdSelectionOutcome anAdSelectionOutcomeInDB(
+            @NonNull AdSelectionEntryDao mAdSelectionEntryDao) {
+        final Uri biddingLogicUri1 = Uri.parse("https://www.domain.com/logic/1");
+        final Uri renderUri = Uri.parse("https://www.domain.com/advert/");
+        final Instant activationTime = Instant.now();
+        final long adSelectionId = 1;
+        final String contextualSignals = "contextual_signals";
+        final double bid1 = 10;
+        final String callerPackageName1 = "callerPackageName1";
+        final CustomAudienceSignals customAudienceSignals =
+                CustomAudienceSignalsFixture.aCustomAudienceSignals();
+        final DBAdSelection dbAdSelectionEntry =
+                new DBAdSelection.Builder()
+                        .setAdSelectionId(adSelectionId)
+                        .setCustomAudienceSignals(customAudienceSignals)
+                        .setContextualSignals(contextualSignals)
+                        .setBiddingLogicUri(biddingLogicUri1)
+                        .setWinningAdRenderUri(renderUri)
+                        .setWinningAdBid(bid1)
+                        .setCreationTimestamp(activationTime)
+                        .setCallerPackageName(callerPackageName1)
+                        .build();
+        mAdSelectionEntryDao.persistAdSelection(dbAdSelectionEntry);
+        return anAdSelectionOutcome(
+                dbAdSelectionEntry.getAdSelectionId(), dbAdSelectionEntry.getWinningAdRenderUri());
     }
 }
