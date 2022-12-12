a006cc2b85e3584b1a76d528e27b63133c2b2953
Hao Liu <haoliuu@google.com>
2022-12-12 23:05:07 +0000

    Add Mdd enrollment data file test.
    The new test verifies Mdd enrollment data downloaded and processed successfully. We need to update this test when a new Mdd enrollment data is uploaded to the server.
    
    Test: atest  AdServicesServiceCoreUnitTests AdServicesFrameworkUnitTests AdServicesEndToEndTests CtsAdServicesEndToEndTests CtsAdServicesDeviceTestCases AdServicesApkUnitTests CtsSandboxedTopicsManagerTests
    
    Bug: 261918103
    Change-Id: I6c5a2cbbe27c143016ea01a829915f2199a81658
    
    Add Mdd enrollment data file test.
    
    The new test verifies Mdd enrollment data downloaded and processed successfully. We need to update this test when a new Mdd enrollment data is uploaded to the server.
    
    Test: atest  AdServicesServiceCoreUnitTests AdServicesFrameworkUnitTests AdServicesEndToEndTests CtsAdServicesEndToEndTests CtsAdServicesDeviceTestCases AdServicesApkUnitTests CtsSandboxedTopicsManagerTests
    
    Bug: 261918103
    Change-Id: I6c5a2cbbe27c143016ea01a829915f2199a81658

--- adservices/tests/unittest/service-core/src/com/android/adservices/download/MobileDataDownloadTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/download/MobileDataDownloadTest.java
@@ -16,17 +16,26 @@
 
 package com.android.adservices.download;
 
+import static com.android.adservices.download.EnrollmentDataDownloadManager.DownloadStatus.SUCCESS;
+
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import android.content.Context;
+import android.database.DatabaseUtils;
 
+import androidx.annotation.NonNull;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.SmallTest;
 
+import com.android.adservices.data.DbHelper;
+import com.android.adservices.data.DbTestUtil;
+import com.android.adservices.data.enrollment.EnrollmentDao;
+import com.android.adservices.data.enrollment.EnrollmentTables;
 import com.android.adservices.service.Flags;
 import com.android.adservices.service.FlagsFactory;
 import com.android.dx.mockito.inline.extended.ExtendedMockito;
@@ -34,11 +43,17 @@ import com.android.dx.mockito.inline.extended.StaticMockitoSession;
 
 import com.google.android.libraries.mobiledatadownload.AddFileGroupRequest;
 import com.google.android.libraries.mobiledatadownload.DownloadFileGroupRequest;
+import com.google.android.libraries.mobiledatadownload.FileGroupPopulator;
 import com.google.android.libraries.mobiledatadownload.GetFileGroupRequest;
 import com.google.android.libraries.mobiledatadownload.Logger;
 import com.google.android.libraries.mobiledatadownload.MobileDataDownload;
+import com.google.android.libraries.mobiledatadownload.MobileDataDownloadBuilder;
 import com.google.android.libraries.mobiledatadownload.TaskScheduler;
+import com.google.android.libraries.mobiledatadownload.downloader.FileDownloader;
+import com.google.android.libraries.mobiledatadownload.file.SynchronousFileStorage;
+import com.google.android.libraries.mobiledatadownload.monitor.NetworkUsageMonitor;
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.google.mobiledatadownload.ClientConfigProto.ClientFileGroup;
 import com.google.mobiledatadownload.DownloadConfigProto.DataFile;
 import com.google.mobiledatadownload.DownloadConfigProto.DataFileGroup;
@@ -50,6 +65,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.quality.Strictness;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -73,14 +89,22 @@ public class MobileDataDownloadTest {
     private static final String FILE_URL_2 = "https://www.gstatic.com/suggest-dev/odws1_empty.jar";
     private static final int FILE_SIZE_2 = 554;
 
+    // TODO(b/263521464): Use the production topics classifier manifest URL.
     private static final String TEST_MDD_TOPICS_CLASSIFIER_MANIFEST_FILE_URL =
             "https://www.gstatic.com/mdi-serving/rubidium-adservices-topics-classifier/922/217081737fd739c74dd3ca5c407813d818526577";
-    private static final String TEST_MDD_ENROLLMENT_MANIFEST_FILE_URL =
+    private static final String MDD_ENROLLMENT_MANIFEST_FILE_URL =
             "https://dl.google.com/mdi-serving/adservices/adtech_enrollment/manifest_configs/1/manifest_config_1658790241927.binaryproto";
+    // Prod Test Bed enrollment manifest URL
+    private static final String PTB_ENROLLMENT_MANIFEST_FILE_URL =
+            "https://www.gstatic.com/mdi-serving/rubidium-adservices-adtech-enrollment/1281/a245b0927ba27b3d954b0ca2775651ccfc9a5e84";
     public static final String TEST_TOPIC_FILE_GROUP_NAME = "topics-classifier-model";
-    public static final String TEST_ENROLLMENT_FILE_GROUP_NAME = "adtech_enrollment_data";
+    public static final String ENROLLMENT_FILE_GROUP_NAME = "adtech_enrollment_data";
 
     private StaticMockitoSession mStaticMockSession = null;
+    private SynchronousFileStorage mFileStorage;
+    private FileDownloader mFileDownloader;
+    private DbHelper mDbHelper;
+
     @Mock Flags mMockFlags;
 
     @Before
@@ -92,7 +116,22 @@ public class MobileDataDownloadTest {
                 ExtendedMockito.mockitoSession()
                         .spyStatic(MddLogger.class)
                         .spyStatic(FlagsFactory.class)
+                        .spyStatic(MobileDataDownloadFactory.class)
+                        .spyStatic(EnrollmentDao.class)
+                        .strictness(Strictness.LENIENT)
                         .startMocking();
+
+        doReturn(/* Download max download threads */ 2)
+                .when(mMockFlags)
+                .getDownloaderMaxDownloadThreads();
+
+        doReturn(/* Default value */ false).when(mMockFlags).getEnableDatabaseSchemaVersion5();
+
+        mFileStorage = MobileDataDownloadFactory.getFileStorage(mContext);
+        mFileDownloader =
+                MobileDataDownloadFactory.getFileDownloader(mContext, mMockFlags, mFileStorage);
+
+        mDbHelper = DbTestUtil.getDbHelperForTest();
     }
 
     @After
@@ -105,8 +144,12 @@ public class MobileDataDownloadTest {
     @Test
     public void testCreateMddManagerSuccessfully() throws ExecutionException, InterruptedException {
         MobileDataDownload mdd =
-                MobileDataDownloadFactory.getMddForTesting(
-                        mContext, FlagsFactory.getFlagsForTest());
+                getMddForTesting(
+                        mContext,
+                        FlagsFactory.getFlagsForTest(),
+                        ImmutableList.<FileGroupPopulator>builder()
+                                .build()); // Pass in an empty list of FileGroupPopulator. Add ad
+        // hoc datafilegroup to MDD manually below.
 
         DataFileGroup dataFileGroup =
                 createDataFileGroup(
@@ -144,30 +187,30 @@ public class MobileDataDownloadTest {
     }
 
     /**
-     * This method tests topics and measurement manifest files. It downloads test classifier model
-     * and adtech enrollment data and verifies files downloaded successfully.
+     * This method tests topics manifest files. It downloads test classifier model and verifies
+     * files downloaded successfully.
      */
     @Test
-    public void testManifestFileGroupPopulator()
+    public void testTopicsManifestFileGroupPopulator()
             throws ExecutionException, InterruptedException, TimeoutException {
-        ExtendedMockito.doReturn(mMockFlags).when(FlagsFactory::getFlagsForTest);
         // Return a manifest URL for test only. This will download smaller size files only for
         // testing MDD download feature.
         doReturn(TEST_MDD_TOPICS_CLASSIFIER_MANIFEST_FILE_URL)
                 .when(mMockFlags)
                 .getMddTopicsClassifierManifestFileUrl();
-        doReturn(/*Download max download threads */ 2)
-                .when(mMockFlags)
-                .getDownloaderMaxDownloadThreads();
-        // Return production enrollment manifest file. This is a small file that will not affect
-        // running this test.
-        doReturn(TEST_MDD_ENROLLMENT_MANIFEST_FILE_URL)
-                .when(mMockFlags)
-                .getMeasurementManifestFileUrl();
+
+        FileGroupPopulator fileGroupPopulator =
+                MobileDataDownloadFactory.getTopicsManifestPopulator(
+                        mContext, mMockFlags, mFileStorage, mFileDownloader);
 
         MobileDataDownload mdd =
-                MobileDataDownloadFactory.getMddForTesting(
-                        mContext, FlagsFactory.getFlagsForTest());
+                getMddForTesting(
+                        mContext,
+                        mMockFlags,
+                        ImmutableList.<FileGroupPopulator>builder()
+                                .add(fileGroupPopulator)
+                                .build()); // List of FileGroupPopulator that contains Topics
+        // FileGroupPopulator only.
 
         mdd.handleTask(TaskScheduler.WIFI_CHARGING_PERIODIC_TASK)
                 .get(MAX_HANDLE_TASK_WAIT_TIME_SECS, SECONDS);
@@ -185,21 +228,117 @@ public class MobileDataDownloadTest {
         assertThat(clientFileGroup.getVersionNumber())
                 .isEqualTo(/* Test filegroup version number */ 0);
         assertThat(clientFileGroup.getFileCount()).isEqualTo(6);
+        assertThat(clientFileGroup.getStatus()).isEqualTo(ClientFileGroup.Status.DOWNLOADED);
         assertThat(clientFileGroup.getBuildId()).isEqualTo(/* BuildID generated by Ingress */ 922);
+    }
 
-        clientFileGroup =
+    /**
+     * This method tests measurement manifest files. It downloads production adtech enrollment data,
+     * verifies files downloaded successfully and data saved into DB correctly.
+     */
+    @Test
+    public void testEnrollmentManifestFileGroupPopulator()
+            throws ExecutionException, InterruptedException, TimeoutException {
+        MobileDataDownload mdd = getMddForEnrollment(MDD_ENROLLMENT_MANIFEST_FILE_URL);
+
+        ClientFileGroup clientFileGroup =
                 mdd.getFileGroup(
                                 GetFileGroupRequest.newBuilder()
-                                        .setGroupName(TEST_ENROLLMENT_FILE_GROUP_NAME)
+                                        .setGroupName(ENROLLMENT_FILE_GROUP_NAME)
                                         .build())
                         .get();
 
         // Verify measurement file group
-        assertThat(clientFileGroup.getGroupName()).isEqualTo(TEST_ENROLLMENT_FILE_GROUP_NAME);
+        assertThat(clientFileGroup.getGroupName()).isEqualTo(ENROLLMENT_FILE_GROUP_NAME);
         assertThat(clientFileGroup.getOwnerPackage()).isEqualTo(mContext.getPackageName());
         assertThat(clientFileGroup.getFileCount()).isEqualTo(1);
+        assertThat(clientFileGroup.getStatus()).isEqualTo(ClientFileGroup.Status.DOWNLOADED);
         assertThat(clientFileGroup.getVersionNumber())
                 .isEqualTo(/* Measurement filegroup version number */ 1);
+
+        ExtendedMockito.doReturn(mdd)
+                .when(() -> MobileDataDownloadFactory.getMdd(any(Context.class), any(Flags.class)));
+
+        EnrollmentDataDownloadManager enrollmentDataDownloadManager =
+                new EnrollmentDataDownloadManager(mContext, mMockFlags);
+
+        EnrollmentDao enrollmentDao = new EnrollmentDao(mContext, mDbHelper);
+
+        ExtendedMockito.doReturn(enrollmentDao)
+                .when(() -> EnrollmentDao.getInstance(any(Context.class)));
+
+        assertThat(enrollmentDao.deleteAll()).isTrue();
+        // Verify no enrollment data after table cleared.
+        assertThat(getNumEntriesInEnrollmentTable()).isEqualTo(0);
+        // Verify enrollment data file read from MDD and insert the data into the enrollment
+        // database.
+        assertThat(enrollmentDataDownloadManager.readAndInsertEnrolmentDataFromMdd().get())
+                .isEqualTo(SUCCESS);
+        // 5 enrollment records added from the MDD data file.
+        assertThat(getNumEntriesInEnrollmentTable()).isEqualTo(5);
+        assertThat(enrollmentDao.deleteAll()).isTrue();
+    }
+
+    /**
+     * This method tests Prod Test Bed measurement manifest files. It downloads Prod Test Bed adtech
+     * enrollment data, verifies files downloaded successfully and data saved into DB correctly.
+     */
+    @Test
+    public void testPtbEnrollmentManifestFileGroupPopulator()
+            throws ExecutionException, InterruptedException, TimeoutException {
+        MobileDataDownload mdd = getMddForEnrollment(PTB_ENROLLMENT_MANIFEST_FILE_URL);
+
+        ClientFileGroup clientFileGroup =
+                mdd.getFileGroup(
+                                GetFileGroupRequest.newBuilder()
+                                        .setGroupName(ENROLLMENT_FILE_GROUP_NAME)
+                                        .build())
+                        .get();
+
+        // Verify measurement file group
+        assertThat(clientFileGroup.getGroupName()).isEqualTo(ENROLLMENT_FILE_GROUP_NAME);
+        assertThat(clientFileGroup.getOwnerPackage()).isEqualTo(mContext.getPackageName());
+        assertThat(clientFileGroup.getFileCount()).isEqualTo(1);
+        assertThat(clientFileGroup.getStatus()).isEqualTo(ClientFileGroup.Status.DOWNLOADED);
+        assertThat(clientFileGroup.getVersionNumber())
+                .isEqualTo(/* PTB Measurement filegroup version number */ 0);
+
+        ExtendedMockito.doReturn(mdd)
+                .when(() -> MobileDataDownloadFactory.getMdd(any(Context.class), any(Flags.class)));
+
+        EnrollmentDataDownloadManager enrollmentDataDownloadManager =
+                new EnrollmentDataDownloadManager(mContext, mMockFlags);
+        EnrollmentDao enrollmentDao = new EnrollmentDao(mContext, mDbHelper);
+
+        ExtendedMockito.doReturn(enrollmentDao)
+                .when(() -> EnrollmentDao.getInstance(any(Context.class)));
+
+        assertThat(enrollmentDao.deleteAll()).isTrue();
+        // Verify no enrollment data after table cleared.
+        assertThat(getNumEntriesInEnrollmentTable()).isEqualTo(0);
+        // Verify enrollment data file read from MDD and insert the data into the enrollment
+        // database.
+        assertThat(enrollmentDataDownloadManager.readAndInsertEnrolmentDataFromMdd().get())
+                .isEqualTo(SUCCESS);
+        // 1 enrollment record added from the PTB MDD data file.
+        assertThat(getNumEntriesInEnrollmentTable()).isEqualTo(1);
+        assertThat(enrollmentDao.deleteAll()).isTrue();
+    }
+
+    @Test
+    public void testMddLoggerKillSwitchIsOn() {
+        // Killswitch is on. MddLogger should be disabled.
+        doReturn(true).when(mMockFlags).getMddLoggerKillSwitch();
+        Optional<Logger> mddLogger = MobileDataDownloadFactory.getMddLogger(mMockFlags);
+        assertThat(mddLogger).isAbsent();
+    }
+
+    @Test
+    public void testMddLoggerKillSwitchIsOff() {
+        // Killswitch is off. MddLogger should be enabled.
+        doReturn(false).when(mMockFlags).getMddLoggerKillSwitch();
+        Optional<Logger> mddLogger = MobileDataDownloadFactory.getMddLogger(mMockFlags);
+        assertThat(mddLogger).isPresent();
     }
 
     // A helper function to create a DataFilegroup.
@@ -241,23 +380,69 @@ public class MobileDataDownloadTest {
         return dataFileGroupBuilder.build();
     }
 
-    @Test
-    public void testMddLoggerKillSwitchIsOn() {
-        ExtendedMockito.doReturn(mMockFlags).when(FlagsFactory::getFlagsForTest);
-        // Killswitch is on. MddLogger should be disabled.
-        doReturn(true).when(mMockFlags).getMddLoggerKillSwitch();
-        Optional<Logger> mddLogger =
-                MobileDataDownloadFactory.getMddLogger(FlagsFactory.getFlagsForTest());
-        assertThat(mddLogger).isAbsent();
+    /**
+     * Returns a MobileDataDownload instance for testing.
+     *
+     * @param context the context
+     * @param flags the flags
+     * @param fileGroupPopulators a list of FileGroupPopulator that will be added to the MDD
+     * @return a MobileDataDownload instance.
+     */
+    @NonNull
+    private MobileDataDownload getMddForTesting(
+            @NonNull Context context,
+            @NonNull Flags flags,
+            @NonNull ImmutableList<FileGroupPopulator> fileGroupPopulators) {
+        context = context.getApplicationContext();
+        SynchronousFileStorage fileStorage = MobileDataDownloadFactory.getFileStorage(context);
+        FileDownloader fileDownloader =
+                MobileDataDownloadFactory.getFileDownloader(context, flags, fileStorage);
+        NetworkUsageMonitor networkUsageMonitor =
+                new NetworkUsageMonitor(context, System::currentTimeMillis);
+
+        return MobileDataDownloadBuilder.newBuilder()
+                .setContext(context)
+                .setControlExecutor(MobileDataDownloadFactory.getControlExecutor())
+                .setNetworkUsageMonitor(networkUsageMonitor)
+                .setFileStorage(fileStorage)
+                .setFileDownloaderSupplier(() -> fileDownloader)
+                .addFileGroupPopulators(fileGroupPopulators)
+                .setLoggerOptional(MobileDataDownloadFactory.getMddLogger(flags))
+                // Use default MDD flags so that it does not need to access DeviceConfig
+                // which is inaccessible from Unit Tests.
+                .setFlagsOptional(
+                        Optional.of(new com.google.android.libraries.mobiledatadownload.Flags() {}))
+                .build();
     }
 
-    @Test
-    public void testMddLoggerKillSwitchIsOff() {
-        ExtendedMockito.doReturn(mMockFlags).when(FlagsFactory::getFlagsForTest);
-        // Killswitch is off. MddLogger should be enabled.
-        doReturn(false).when(mMockFlags).getMddLoggerKillSwitch();
-        Optional<Logger> mddLogger =
-                MobileDataDownloadFactory.getMddLogger(FlagsFactory.getFlagsForTest());
-        assertThat(mddLogger).isPresent();
+    // Returns MobileDataDownload using passed in enrollment manifest url.
+    @NonNull
+    private MobileDataDownload getMddForEnrollment(String enrollmentManifestFileUrl)
+            throws ExecutionException, InterruptedException, TimeoutException {
+        doReturn(enrollmentManifestFileUrl).when(mMockFlags).getMeasurementManifestFileUrl();
+
+        FileGroupPopulator fileGroupPopulator =
+                MobileDataDownloadFactory.getMeasurementManifestPopulator(
+                        mContext, mMockFlags, mFileStorage, mFileDownloader);
+
+        MobileDataDownload mdd =
+                getMddForTesting(
+                        mContext,
+                        mMockFlags,
+                        ImmutableList.<FileGroupPopulator>builder()
+                                .add(fileGroupPopulator)
+                                .build()); // List of FileGroupPopulator that contains Measurement
+        // FileGroupPopulator only.
+
+        mdd.handleTask(TaskScheduler.WIFI_CHARGING_PERIODIC_TASK)
+                .get(MAX_HANDLE_TASK_WAIT_TIME_SECS, SECONDS);
+        return mdd;
+    }
+
+    private long getNumEntriesInEnrollmentTable() {
+        return DatabaseUtils.queryNumEntries(
+                mDbHelper.getReadableDatabase(),
+                EnrollmentTables.EnrollmentDataContract.TABLE,
+                null);
     }
 }
