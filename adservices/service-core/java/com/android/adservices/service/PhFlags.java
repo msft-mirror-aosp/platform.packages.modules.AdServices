485f756dfdbbd6ef3afb9141b508ddda56615d74
Binh Nguyen <binhnguyen@google.com>
2022-12-20 22:29:33 +0000

    Revert "[automerge] Setup default flags for emulators 2p: d187d29233"
    Revert submission 20727064-presubmit-am-0411b80f1cb4415ea62b15e940a30829
    
    Reason for revert: We are not be able to disable PPAPI on emulators via PH.
    
    Reverted changes: /q/submissionid:20727064-presubmit-am-0411b80f1cb4415ea62b15e940a30829
    
    Change-Id: I718ca4ac01a8b9a4ae67a5523dc1cdf1630567b2

--- adservices/service-core/java/com/android/adservices/service/PhFlags.java
+++ adservices/service-core/java/com/android/adservices/service/PhFlags.java
@@ -1003,18 +1003,10 @@ public final class PhFlags implements Flags {
         // hard-coded value.
         return SystemProperties.getBoolean(
                 getSystemPropertyName(KEY_GLOBAL_KILL_SWITCH),
-                /* defaultValue */
-                isEmulator()
-                        ? false
-                        : DeviceConfig.getBoolean(
-                                DeviceConfig.NAMESPACE_ADSERVICES,
-                                /* flagName */ KEY_GLOBAL_KILL_SWITCH,
-                                /* defaultValue */ GLOBAL_KILL_SWITCH));
-    }
-
-    // Return if the device is an emulator or not.
-    private boolean isEmulator() {
-        return SystemProperties.getBoolean("ro.boot.qemu", false);
+                /* defaultValue */ DeviceConfig.getBoolean(
+                        DeviceConfig.NAMESPACE_ADSERVICES,
+                        /* flagName */ KEY_GLOBAL_KILL_SWITCH,
+                        /* defaultValue */ GLOBAL_KILL_SWITCH));
     }
 
     // MEASUREMENT Killswitches
@@ -1484,12 +1476,10 @@ public final class PhFlags implements Flags {
             return false;
         }
         // The priority of applying the flag values: PH (DeviceConfig) and then hard-coded value.
-        return isEmulator()
-                ? true
-                : DeviceConfig.getBoolean(
-                        DeviceConfig.NAMESPACE_ADSERVICES,
-                        /* flagName */ KEY_ADSERVICES_ENABLED,
-                        /* defaultValue */ ADSERVICES_ENABLED);
+        return DeviceConfig.getBoolean(
+                DeviceConfig.NAMESPACE_ADSERVICES,
+                /* flagName */ KEY_ADSERVICES_ENABLED,
+                /* defaultValue */ ADSERVICES_ENABLED);
     }
 
     @Override
