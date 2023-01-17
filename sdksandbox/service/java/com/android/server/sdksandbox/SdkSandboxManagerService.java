ebe5d1cfe3f5399c3eab360ed5d91b9b20564e54
Palak Chaudhary <cryptic@google.com>
2023-01-17 09:12:31 +0000

    Create hook for checking the restrictions on SDKs intended for the SDK runtime declaring broadcastreceivers through their manifest.
    Bug: b/265754224
    Test: atest SdkSandboxManagerServiceUnitTest
    Change-Id: I72a1f2811065b315270d74af374190f9ec086de2

--- sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
+++ sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
@@ -1931,5 +1931,30 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
                 Binder.restoreCallingIdentity(token);
             }
         }
+
+        @Override
+        // TODO(b/265777283): Use minTargetSdkVersion and intentFilter parameter when using an
+        // allowlist
+        public boolean canDeclareBroadcastReceiverFromManifest(
+                @NonNull IntentFilter intentFilter,
+                boolean unexportedBroadcast,
+                boolean onlyProtectedBroadcasts,
+                int minTargetSdkVersion) {
+            /**
+             * By clearing the calling identity, system server identity is set which allows us to
+             * call {@DeviceConfig.getBoolean}
+             */
+            final long token = Binder.clearCallingIdentity();
+            try {
+                return unexportedBroadcast
+                        || onlyProtectedBroadcasts
+                        || (!DeviceConfig.getBoolean(
+                                DeviceConfig.NAMESPACE_ADSERVICES,
+                                PROPERTY_ENFORCE_BROADCAST_RECEIVER_RESTRICTIONS,
+                                DEFAULT_VALUE_ENFORCE_BROADCAST_RECEIVER_RESTRICTIONS));
+            } finally {
+                Binder.restoreCallingIdentity(token);
+            }
+        }
     }
 }
