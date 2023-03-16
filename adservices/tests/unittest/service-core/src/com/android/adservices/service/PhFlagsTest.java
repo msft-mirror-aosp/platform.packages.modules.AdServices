138e13a11d1c778167167eaebf53ffbb204c0bbf
Zeyad El-arabaty <zelarabaty@google.com>
2023-03-16 13:38:14 -0700

    Added CTS tests for reportInteraction
    MUST_SLEEP
    
    Test: atest CtsAdServicesDebuggableDeviceTestCases
    
    Change-Id: I1d5f9f0df7193858e55a461cb180bb983bd323eb

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/PhFlagsTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/PhFlagsTest.java
@@ -234,7 +234,6 @@ import static com.android.adservices.service.PhFlags.KEY_FLEDGE_HTTP_CACHE_DEFAU
 import static com.android.adservices.service.PhFlags.KEY_FLEDGE_HTTP_CACHE_ENABLE;
 import static com.android.adservices.service.PhFlags.KEY_FLEDGE_HTTP_CACHE_ENABLE_JS_CACHING;
 import static com.android.adservices.service.PhFlags.KEY_FLEDGE_HTTP_CACHE_MAX_ENTRIES;
-import static com.android.adservices.service.PhFlags.KEY_FLEDGE_REGISTER_AD_BEACON_ENABLED;
 import static com.android.adservices.service.PhFlags.KEY_FLEDGE_REPORT_IMPRESSION_MAX_REGISTERED_AD_BEACONS_PER_AD_TECH_COUNT;
 import static com.android.adservices.service.PhFlags.KEY_FLEDGE_REPORT_IMPRESSION_MAX_REGISTERED_AD_BEACONS_TOTAL_COUNT;
 import static com.android.adservices.service.PhFlags.KEY_FLEDGE_REPORT_IMPRESSION_OVERALL_TIMEOUT_MS;
@@ -1773,11 +1772,7 @@ public class PhFlagsTest {
 
         final boolean phOverridingValue = !FLEDGE_REGISTER_AD_BEACON_ENABLED;
 
-        DeviceConfig.setProperty(
-                DeviceConfig.NAMESPACE_ADSERVICES,
-                KEY_FLEDGE_REGISTER_AD_BEACON_ENABLED,
-                Boolean.toString(phOverridingValue),
-                /* makeDefault */ false);
+        PhFlagsFixture.overrideFledgeRegisterAdBeaconEnabled(phOverridingValue);
 
         Flags phFlags = FlagsFactory.getFlags();
         assertThat(phFlags.getFledgeRegisterAdBeaconEnabled()).isEqualTo(phOverridingValue);
