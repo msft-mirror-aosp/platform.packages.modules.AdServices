092c7c93988b14b5dab76c519db9f42c8805042f
Yang Wang <yangwangyw@google.com>
2023-03-31 14:03:48 +0000

    Update logs to differentiate flip flow vs normal flow.
    Test: atest
    Bug: b/276360105
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4d6311a7ac811f1256183534b582b73d79ad2c0e)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0627b5dcfeb9e7a24720f4305b66324e9edaae06)
    Merged-In: I32238d0d6b58a4a4027b45b3b961bc2e05187047
    Change-Id: I32238d0d6b58a4a4027b45b3b961bc2e05187047

--- adservices/apk/tests/src/com/android/adservices/ui/notifications/ConsentNotificationTriggerTest.java
+++ adservices/apk/tests/src/com/android/adservices/ui/notifications/ConsentNotificationTriggerTest.java
@@ -111,7 +111,7 @@ public class ConsentNotificationTriggerTest {
         ExtendedMockito.doReturn(mAdServicesLoggerImpl).when(AdServicesLoggerImpl::getInstance);
         doReturn(mAdServicesManager).when(mContext).getSystemService(AdServicesManager.class);
         doReturn(mConsentManager).when(() -> ConsentManager.getInstance(any(Context.class)));
-        doReturn(false).when(mMockFlags).isUiFeatureTypeLoggingEnabled();
+        doReturn(true).when(mMockFlags).isUiFeatureTypeLoggingEnabled();
         doReturn(true).when(mMockFlags).getNotificationDismissedOnClick();
         doReturn(false).when(mMockFlags).getEuNotifFlowChangeEnabled();
         cancelAllPreviousNotifications();
