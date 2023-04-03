8ba644ca370f3bca775f5ee5303537588d7f5688
Samiul Islam <samiul@google.com>
2023-04-03 12:29:14 +0100

    Wait for sdk directories to be created after install
    Bug: 276544991
    Test: atest SdkSandboxStorageHostTest
    Change-Id: I0cfba3622ae1076d86e2e8b79038a7ad9dcc9387

--- sdksandbox/tests/hostsidetests/SdkSandboxStorageHostTest/src/com/android/tests/sdksandbox/host/SdkSandboxStorageHostTest.java
+++ sdksandbox/tests/hostsidetests/SdkSandboxStorageHostTest/src/com/android/tests/sdksandbox/host/SdkSandboxStorageHostTest.java
@@ -198,6 +198,7 @@ public final class SdkSandboxStorageHostTest extends BaseHostJUnit4Test {
 
         // Install the app
         installPackage(TEST_APP_STORAGE_APK);
+        waitForSdkDirectoryCreatedForUser(0);
 
         // Verify directory is created
         assertThat(getDevice().isDirectory(cePath)).isTrue();
