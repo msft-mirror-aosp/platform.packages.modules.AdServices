1c7c0f0482071f52f10e8e331dd35b4ac9a457cb
Adarsh Sridhar <adarshsridhar@google.com>
2023-04-27 22:23:20 -0700

    Implement measurement rollback on Android S
    Since we do not have access to System Server on S, we instead store data in AppSearch. Also, we don't have the necessary MANAGE_ROLLBACKS permission to use `RollbackManager` to query recent mainline train rollbacks. Therefore, we instead store the current apex version in a document in AppSearch when a deletion event occurs, and compare this stored value to the current apex version on Measurement service start. If the stored value is higher, then it indicates that a deletion event happened in a newer apex version, which means that a rollback happened and the measurement db needs to be cleared.
    
    Bug: 280727787
    Test: atest AdExtServicesServiceCoreUnitTests
    Test: atest AdServicesEndToEndTests
    Test: atest CtsAdServicesEndToEndTestMeasurement
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0cbda62a6fe854eb9f8aeea2cb7fb1b4ab99f36d)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e50c9c6de8ceb706d9f2881bb922665e1c8b6b8c)
    Merged-In: Ic2a608508dab99a9103ce007076ca750f14facc4
    Change-Id: Ic2a608508dab99a9103ce007076ca750f14facc4

--- adservices/service-core/java/com/android/adservices/service/PhFlags.java
+++ adservices/service-core/java/com/android/adservices/service/PhFlags.java
@@ -314,6 +314,9 @@ public final class PhFlags implements Flags {
             "measurement_job_registration_fallback_job_kill_switch";
     static final String KEY_MEASUREMENT_ROLLBACK_DELETION_KILL_SWITCH =
             "measurement_rollback_deletion_kill_switch";
+
+    static final String KEY_MEASUREMENT_ROLLBACK_DELETION_APP_SEARCH_KILL_SWITCH =
+            "measurement_rollback_deletion_app_search_kill_switch";
     static final String KEY_TOPICS_KILL_SWITCH = "topics_kill_switch";
     static final String KEY_TOPICS_ON_DEVICE_CLASSIFIER_KILL_SWITCH =
             "topics_on_device_classifier_kill_switch";
@@ -1640,6 +1643,17 @@ public final class PhFlags implements Flags {
                                 NAMESPACE_ADSERVICES,
                                 /* flagName */ KEY_MEASUREMENT_ROLLBACK_DELETION_KILL_SWITCH,
                                 defaultValue));
+    }
+
+    @Override
+    public boolean getMeasurementRollbackDeletionAppSearchKillSwitch() {
+        final boolean defaultValue = MEASUREMENT_ROLLBACK_DELETION_APP_SEARCH_KILL_SWITCH;
+        return SystemProperties.getBoolean(
+                getSystemPropertyName(KEY_MEASUREMENT_ROLLBACK_DELETION_APP_SEARCH_KILL_SWITCH),
+                /* def= */ DeviceConfig.getBoolean(
+                        NAMESPACE_ADSERVICES,
+                        /* name= */ KEY_MEASUREMENT_ROLLBACK_DELETION_APP_SEARCH_KILL_SWITCH,
+                        defaultValue));
     }
 
     @Override
@@ -3050,6 +3064,11 @@ public final class PhFlags implements Flags {
         writer.println("\t" + KEY_ENABLE_BACK_COMPAT + " = " + getEnableBackCompat());
         writer.println(
                 "\t" + KEY_ENABLE_APPSEARCH_CONSENT_DATA + " = " + getEnableAppsearchConsentData());
+        writer.println(
+                "\t"
+                        + KEY_MEASUREMENT_ROLLBACK_DELETION_APP_SEARCH_KILL_SWITCH
+                        + " = "
+                        + getMeasurementRollbackDeletionAppSearchKillSwitch());
     }
 
     @Override
