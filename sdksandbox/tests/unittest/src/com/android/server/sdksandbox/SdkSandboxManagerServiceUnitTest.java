b77dfa872fe78a3ac6eb15d052bb90097686692c
Felipe Leme <felipeal@google.com>
2023-05-12 16:45:19 -0700

    Publishes adservices_manager service.
    This service was previously unpublish because it was not allowlisted
    by some OEMs (and the workaround was to provide it to apps through
    SdkSandboxManager); this CL re-adds the service, although it keeps
    the SdkSandboxManager hook-up (to make sure it still doesn't break
    phones through mainline update; it will be removed later, on VIC).
    
    Test: adb shell dumpsys system_server_dumper --list |grep AdServices && echo FAIL || echo SUCCESS
    Test: adb shell service check adservices_manager | grep "not found" > /dev/null && echo FAIL || echo SUCCESS
    Test: atest SdkSandboxManagerServiceUnitTest CtsAdServicesDeviceTestCases:android.adservices.cts.AdServicesJUnit4DeviceTest#testBinderServiceIsPublished AdServicesManagerServiceLifeCycleTest
    
    Bug: 282239822
    
    Change-Id: Ib81dd850c12c461cbb2515d56cec0d992d33a666

--- sdksandbox/tests/unittest/src/com/android/server/sdksandbox/SdkSandboxManagerServiceUnitTest.java
+++ sdksandbox/tests/unittest/src/com/android/server/sdksandbox/SdkSandboxManagerServiceUnitTest.java
@@ -1725,10 +1725,10 @@ public class SdkSandboxManagerServiceUnitTest {
     }
 
     @Test
-    public void testDump_preU() throws Exception {
+    public void testDump_preU_notPublished() throws Exception {
         requiresAtLeastU(false);
         mockGrantedPermission(DUMP);
-        mService.registerAdServicesManagerService(mAdServicesManager);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ false);
 
         String dump;
         try (StringWriter stringWriter = new StringWriter()) {
@@ -1754,10 +1754,29 @@ public class SdkSandboxManagerServiceUnitTest {
     }
 
     @Test
-    public void testDump_atLeastU() throws Exception {
+    public void testDump_preU_published() throws Exception {
+        requiresAtLeastU(false);
+        mockGrantedPermission(DUMP);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ true);
+
+        String dump;
+        try (StringWriter stringWriter = new StringWriter()) {
+            mService.dump(new FileDescriptor(), new PrintWriter(stringWriter), new String[0]);
+
+            dump = stringWriter.toString();
+        }
+
+        assertThat(dump).contains("FakeDump");
+
+        Mockito.verify(mAdServicesManager, Mockito.never())
+                .dump(ArgumentMatchers.any(), ArgumentMatchers.any());
+    }
+
+    @Test
+    public void testDump_atLeastU_notPublished() throws Exception {
         requiresAtLeastU(true);
         mockGrantedPermission(DUMP);
-        mService.registerAdServicesManagerService(mAdServicesManager);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ false);
 
         String dump;
         try (StringWriter stringWriter = new StringWriter()) {
@@ -1772,10 +1791,28 @@ public class SdkSandboxManagerServiceUnitTest {
     }
 
     @Test
-    public void testDump_adServices_preU() throws Exception {
+    public void testDump_atLeastU_published() throws Exception {
+        requiresAtLeastU(true);
+        mockGrantedPermission(DUMP);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ true);
+
+        String dump;
+        try (StringWriter stringWriter = new StringWriter()) {
+            mService.dump(new FileDescriptor(), new PrintWriter(stringWriter), new String[0]);
+            dump = stringWriter.toString();
+        }
+
+        assertThat(dump).contains("FakeDump");
+
+        Mockito.verify(mAdServicesManager, Mockito.never())
+                .dump(ArgumentMatchers.any(), ArgumentMatchers.any());
+    }
+
+    @Test
+    public void testDump_adServices_preU_notPublished() throws Exception {
         requiresAtLeastU(false);
         mockGrantedPermission(DUMP);
-        mService.registerAdServicesManagerService(mAdServicesManager);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ false);
 
         String dump;
         try (StringWriter stringWriter = new StringWriter()) {
@@ -1800,10 +1837,10 @@ public class SdkSandboxManagerServiceUnitTest {
     }
 
     @Test
-    public void testDump_adServices_atLeastU() throws Exception {
-        requiresAtLeastU(true);
+    public void testDump_adServices_preU_published() throws Exception {
+        requiresAtLeastU(false);
         mockGrantedPermission(DUMP);
-        mService.registerAdServicesManagerService(mAdServicesManager);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ true);
 
         String dump;
         try (StringWriter stringWriter = new StringWriter()) {
@@ -1815,7 +1852,58 @@ public class SdkSandboxManagerServiceUnitTest {
         }
 
         assertThat(dump)
-                .isEqualTo(SdkSandboxManagerService.POST_UDC_DUMP_AD_SERVICES_MESSAGE + "\n");
+                .isEqualTo(
+                        SdkSandboxManagerService
+                                        .DUMP_AD_SERVICES_MESSAGE_HANDLED_BY_AD_SERVICES_ITSELF
+                                + "\n");
+        Mockito.verify(mAdServicesManager, Mockito.never())
+                .dump(ArgumentMatchers.any(), ArgumentMatchers.any());
+    }
+
+    @Test
+    public void testDump_adServices_atLeastU_notPublished() throws Exception {
+        requiresAtLeastU(true);
+        mockGrantedPermission(DUMP);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ false);
+
+        String dump;
+        try (StringWriter stringWriter = new StringWriter()) {
+            mService.dump(
+                    new FileDescriptor(),
+                    new PrintWriter(stringWriter),
+                    new String[] {"--AdServices"});
+            dump = stringWriter.toString();
+        }
+
+        assertThat(dump)
+                .isEqualTo(
+                        SdkSandboxManagerService.DUMP_AD_SERVICES_MESSAGE_HANDLED_BY_SYSTEM_SERVICE
+                                + "\n");
+
+        Mockito.verify(mAdServicesManager, Mockito.never())
+                .dump(ArgumentMatchers.any(), ArgumentMatchers.any());
+    }
+
+    @Test
+    public void testDump_adServices_atLeastU_published() throws Exception {
+        requiresAtLeastU(true);
+        mockGrantedPermission(DUMP);
+        mService.registerAdServicesManagerService(mAdServicesManager, /* published= */ true);
+
+        String dump;
+        try (StringWriter stringWriter = new StringWriter()) {
+            mService.dump(
+                    new FileDescriptor(),
+                    new PrintWriter(stringWriter),
+                    new String[] {"--AdServices"});
+            dump = stringWriter.toString();
+        }
+
+        assertThat(dump)
+                .isEqualTo(
+                        SdkSandboxManagerService
+                                        .DUMP_AD_SERVICES_MESSAGE_HANDLED_BY_AD_SERVICES_ITSELF
+                                + "\n");
 
         Mockito.verify(mAdServicesManager, Mockito.never())
                 .dump(ArgumentMatchers.any(), ArgumentMatchers.any());
