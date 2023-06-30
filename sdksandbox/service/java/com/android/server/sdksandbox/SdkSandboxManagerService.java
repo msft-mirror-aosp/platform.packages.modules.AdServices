75a44389c48e70fe4e6470ed9a180c778ade4eb2
Rishabh Singh <rishabhsing@google.com>
2023-06-30 13:52:13 +0000

    Allowing SDK to load SDKs
    This cl adds API in the SdkSandboxController to be
    used by SDKs to load other SDKs
    
    Bug: 289509098
    Test: atest SdkSandboxMediationHostTest#testLoadSdkBySdk
    Change-Id: Ib3a6ada124cc01b0c97ba1cbd7a6cad4fdfbbfee

--- sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
+++ sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
@@ -768,9 +768,18 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
             final long timeSystemServerReceivedCallFromApp = mInjector.getCurrentTime();
 
             final int callingUid = Binder.getCallingUid();
-            final CallingInfo callingInfo =
-                    CallingInfo.fromBinderWithApplicationThread(
-                            mContext, callingPackageName, callingAppProcessToken);
+            CallingInfo callingInfo;
+            if (Process.isSdkSandboxUid(callingUid)) {
+                callingInfo =
+                        CallingInfo.fromExternal(
+                                mContext,
+                                Process.getAppUidForSdkSandboxUid(callingUid),
+                                callingPackageName);
+            } else {
+                callingInfo =
+                        CallingInfo.fromBinderWithApplicationThread(
+                                mContext, callingPackageName, callingAppProcessToken);
+            }
             enforceCallerHasNetworkAccess(callingPackageName);
             enforceCallerOrItsSandboxRunInForeground(callingInfo);
             synchronized (mLock) {
@@ -881,19 +890,24 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
         }
 
         synchronized (mLock) {
-            mUidImportanceListener.startListening();
-            if (!registerForAppDeath(callingInfo, callback.asBinder())) {
-                // Log the time taken in System Server before the exception occurred
-                SdkSandboxStatsLog.write(
-                        SdkSandboxStatsLog.SANDBOX_API_CALLED,
-                        SdkSandboxStatsLog.SANDBOX_API_CALLED__METHOD__LOAD_SDK,
-                        /*latency=*/ (int)
-                                (mInjector.getCurrentTime() - timeSystemServerReceivedCallFromApp),
-                        /*success=*/ false,
-                        SANDBOX_API_CALLED__STAGE__SYSTEM_SERVER_APP_TO_SANDBOX,
-                        callingInfo.getUid());
-                // App has already died and there is no point in loading the SDK.
-                return;
+            if (!callingInfo.isCallFromSdkSandbox()) {
+                // The code is used to be able to detect app death and app foreground state.
+                // Hence, it is of no use for the call from sandbox.
+                mUidImportanceListener.startListening();
+                if (!registerForAppDeath(callingInfo, callback.asBinder())) {
+                    // Log the time taken in System Server before the exception occurred
+                    SdkSandboxStatsLog.write(
+                            SdkSandboxStatsLog.SANDBOX_API_CALLED,
+                            SdkSandboxStatsLog.SANDBOX_API_CALLED__METHOD__LOAD_SDK,
+                            /*latency=*/ (int)
+                                    (mInjector.getCurrentTime()
+                                            - timeSystemServerReceivedCallFromApp),
+                            /*success=*/ false,
+                            SANDBOX_API_CALLED__STAGE__SYSTEM_SERVER_APP_TO_SANDBOX,
+                            callingInfo.getUid());
+                    // App has already died and there is no point in loading the SDK.
+                    return;
+                }
             }
         }
 
@@ -945,15 +959,19 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
             int sandboxStatus = mServiceProvider.getSandboxStatusForApp(callingInfo);
 
             // Check if service is already created for the app.
-            if (sandboxStatus == SdkSandboxServiceProvider.NON_EXISTENT
-                    || sandboxStatus == SdkSandboxServiceProvider.CREATE_PENDING) {
-                addSandboxBindingCallback(callingInfo, callback);
-                if (sandboxStatus == SdkSandboxServiceProvider.NON_EXISTENT) {
-                    isSandboxStartRequired = true;
-                } else {
-                    // The sandbox is in the process of being brought up. Nothing more to do here.
+            if (sandboxStatus == SdkSandboxServiceProvider.NON_EXISTENT) {
+                // We do not want to start sandbox if the call is from sandbox
+                // and sandbox is dead/non-existent since SDK loading the other
+                // SDK itself will be unloaded if sandbox dies after the loadSdk call.
+                if (callingInfo.isCallFromSdkSandbox()) {
                     return;
                 }
+                addSandboxBindingCallback(callingInfo, callback);
+                isSandboxStartRequired = true;
+            } else if (sandboxStatus == SdkSandboxServiceProvider.CREATE_PENDING) {
+                addSandboxBindingCallback(callingInfo, callback);
+                // The sandbox is in the process of being brought up. Nothing more to do here.
+                return;
             }
         }
 
@@ -2399,6 +2417,27 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
                 }
             }
             return sandboxedSdks;
+        }
+
+        @Override
+        public void loadSdk(
+                String callingPackageName,
+                String sdkName,
+                long timeAppCalledSystemServer,
+                Bundle params,
+                ILoadSdkCallback callback)
+                throws RemoteException {
+            // The process token is only used to kill the app process when the
+            // sandbox dies (for U+, not available on T), so a sandbox process token
+            // is not needed here. This is taken care of when the first SDK is loaded
+            // by the app.
+            SdkSandboxManagerService.this.loadSdk(
+                    callingPackageName,
+                    /*callingAppProcessToken=*/ null,
+                    sdkName,
+                    timeAppCalledSystemServer,
+                    params,
+                    callback);
         }
 
         @Override
