236126beafffb32e0ea23184a554ca61932e167b
Alex Pankhurst <pankhurst@google.com>
2023-07-06 01:22:48 +0000

    Remove SystemApi annotations from AdServices Cobalt APIs
    The APIs were not approved before the M09 freeze and need to be released
    with M11
    
    Test: N/A, API annotations
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4b2d61ff2446270c6ee9fbc065a918d31c8633b6)
    Merged-In: Ifdea668a7988bf4c30e1bc5f390e0ccdf9a9a0fc
    Change-Id: Ifdea668a7988bf4c30e1bc5f390e0ccdf9a9a0fc

--- adservices/framework/java/android/adservices/cobalt/EncryptedCobaltEnvelopeParams.java
+++ adservices/framework/java/android/adservices/cobalt/EncryptedCobaltEnvelopeParams.java
@@ -18,7 +18,6 @@ package android.adservices.cobalt;
 
 import android.annotation.IntDef;
 import android.annotation.NonNull;
-import android.annotation.SystemApi;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -30,7 +29,6 @@ import java.lang.annotation.RetentionPolicy;
  *
  * @hide
  */
-@SystemApi
 public final class EncryptedCobaltEnvelopeParams implements Parcelable {
     /**
      * Whether data is from a development or production device.
