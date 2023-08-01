83434967a5f43a13742add312548958983acc669
Albert Kong <albertkong@google.com>
2023-08-01 21:33:47 +0000

    Added check for unsupported UX to ensure UX States are ready.
    Test: atest
    Bug: 294114477
    Fixes: 294114477
    Change-Id: I74de6cf812129f7fec31cfa0b93433a733f21cc0

--- adservices/apk/java/com/android/adservices/ui/settings/activities/AppsActivity.java
+++ adservices/apk/java/com/android/adservices/ui/settings/activities/AppsActivity.java
@@ -15,6 +15,8 @@
  */
 package com.android.adservices.ui.settings.activities;
 
+import static com.android.adservices.ui.UxUtil.isUxStatesReady;
+
 import android.os.Build;
 import android.os.Bundle;
 
@@ -22,8 +24,6 @@ import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.android.adservices.api.R;
-import com.android.adservices.service.FlagsFactory;
-import com.android.adservices.service.consent.ConsentManager;
 import com.android.adservices.ui.settings.activitydelegates.AppsActivityActionDelegate;
 import com.android.adservices.ui.settings.delegates.AppsActionDelegate;
 import com.android.adservices.ui.settings.fragments.AdServicesSettingsAppsFragment;
@@ -46,8 +46,7 @@ public class AppsActivity extends AdServicesBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!FlagsFactory.getFlags().getEnableAdServicesSystemApi()
-                && ConsentManager.getInstance(this).getUx() != null) {
+        if (!isUxStatesReady(this)) {
             initFragment();
         }
     }
