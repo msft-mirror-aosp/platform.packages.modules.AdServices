f1260101c2024e8593d06ac68fa5ce3b9024f520
Felipe Leme <felipeal@google.com>
2023-08-14 17:18:24 -0700

    Moved all flags constants to FlagsConstants.
    That class will be used in multiple places:
    - Production code itself (Flags / PhFlags)
    - Device-side tests
    - Host-side tests
    
    Test: atest AdServicesServiceCoreUnitTests:com.android.adservices.service.PhFlagsTest
    
    Bug: 295321663
    
    Change-Id: I3e243591222c47eb60962e38775e8c422816b616

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/ui/util/UxEngineUtilTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/ui/util/UxEngineUtilTest.java
@@ -16,10 +16,10 @@
 
 package com.android.adservices.service.ui.util;
 
-import static com.android.adservices.service.PhFlags.KEY_ADSERVICES_ENABLED;
-import static com.android.adservices.service.PhFlags.KEY_CONSENT_NOTIFICATION_DEBUG_MODE;
-import static com.android.adservices.service.PhFlags.KEY_GA_UX_FEATURE_ENABLED;
-import static com.android.adservices.service.PhFlags.KEY_U18_UX_ENABLED;
+import static com.android.adservices.service.FlagsConstants.KEY_ADSERVICES_ENABLED;
+import static com.android.adservices.service.FlagsConstants.KEY_CONSENT_NOTIFICATION_DEBUG_MODE;
+import static com.android.adservices.service.FlagsConstants.KEY_GA_UX_FEATURE_ENABLED;
+import static com.android.adservices.service.FlagsConstants.KEY_U18_UX_ENABLED;
 import static com.android.adservices.service.consent.ConsentManager.MANUAL_INTERACTIONS_RECORDED;
 import static com.android.adservices.service.consent.ConsentManager.NO_MANUAL_INTERACTIONS_RECORDED;
 import static com.android.adservices.service.consent.ConsentManager.UNKNOWN;
