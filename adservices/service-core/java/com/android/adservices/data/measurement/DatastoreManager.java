5b6884347fb4dd7c39d37715a64052bc0d44b0ce
Feifei Ji <feifeiji@google.com>
2023-08-18 18:25:26 +0000

    Logging DatastoreManager failures.
    Change-Id: I163ed4e5e5831a5dcf13618ab37dc849bf2634d7
    Test: com.android.adservices.data.measurement
    Bug: 296318383

--- adservices/service-core/java/com/android/adservices/data/measurement/DatastoreManager.java
+++ adservices/service-core/java/com/android/adservices/data/measurement/DatastoreManager.java
@@ -16,8 +16,12 @@
 
 package com.android.adservices.data.measurement;
 
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__ERROR_CODE__MEASUREMENT_DATASTORE_FAILURE;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__ERROR_CODE__MEASUREMENT_DATASTORE_UNKNOWN_FAILURE;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__MEASUREMENT;
 
 import com.android.adservices.LogUtil;
+import com.android.adservices.errorlogging.ErrorLogUtil;
 import com.android.internal.annotations.VisibleForTesting;
 
 import java.util.Optional;
@@ -91,11 +95,19 @@ public abstract class DatastoreManager {
             result = Optional.empty();
             safePrintDataStoreVersion();
             LogUtil.e(ex, "DatastoreException thrown during transaction");
+            ErrorLogUtil.e(
+                    ex,
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__MEASUREMENT_DATASTORE_FAILURE,
+                    AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__MEASUREMENT);
             transaction.rollback();
         } catch (Exception ex) {
             // Catch all exceptions for rollback
             safePrintDataStoreVersion();
             LogUtil.e(ex, "Unhandled exception thrown during transaction");
+            ErrorLogUtil.e(
+                    ex,
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__MEASUREMENT_DATASTORE_UNKNOWN_FAILURE,
+                    AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__MEASUREMENT);
             transaction.rollback();
             throw ex;
         } finally {
