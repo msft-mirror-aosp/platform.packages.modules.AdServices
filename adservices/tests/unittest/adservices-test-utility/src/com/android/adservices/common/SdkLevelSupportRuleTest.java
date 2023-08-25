6976697b2188b99a8983114352fe034fd44579e0
Felipe Leme <felipeal@google.com>
2023-08-25 16:54:51 -0700

    Initial unit tests for AbstractSdkLevelSupportedRule.
    Test: m -j AdServicesTestUtilityTests && adb install -t -g $OUT/testcases/AdServicesTestUtilityTests/*/AdServicesTestUtilityTests.apk && adb shell am instrument -w --no-hidden-api-checks -w com.android.adservices.testutil.tests/androidx.test.runner.AndroidJUnitRunner -e class com.android.adservices.common.SdkLevelSupportRuleTest # atest doesn't work
    Test: m RUN_ERROR_PRONE=true  AdServicesTestUtilityTests
    
    Bug: 295269584
    
    Change-Id: Ibb314998b1b8a011eef4231e3c3dd93349d5e666

---
+++ adservices/tests/unittest/adservices-test-utility/src/com/android/adservices/common/SdkLevelSupportRuleTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.adservices.common;
+
+import static com.android.adservices.mockito.ExtendedMockitoExpectations.mockIsAtLeastS;
+import static com.android.adservices.mockito.ExtendedMockitoExpectations.mockIsAtLeastSv2;
+import static com.android.adservices.mockito.ExtendedMockitoExpectations.mockIsAtLeastT;
+
+import android.util.Log;
+
+import com.android.adservices.common.AbstractSdkLevelSupportedRule.AndroidSdkLevel;
+import com.android.adservices.mockito.AdServicesExtendedMockitoRule;
+import com.android.modules.utils.build.SdkLevel;
+
+import org.junit.Rule;
+
+public final class SdkLevelSupportRuleTest
+        extends AbstractSdkLevelSupportedRuleTestCase<SdkLevelSupportRule> {
+
+    private static final String TAG = SdkLevelSupportRuleTest.class.getSimpleName();
+
+    @Rule
+    public final AdServicesExtendedMockitoRule adServicesExtendedMockitoRule =
+            new AdServicesExtendedMockitoRule.Builder().spyStatic(SdkLevel.class).build();
+
+    @Override
+    protected SdkLevelSupportRule newRuleForAtLeast(AndroidSdkLevel level) {
+        return new SdkLevelSupportRule(level);
+    }
+
+    @Override
+    protected void setDeviceSdkLevel(AndroidSdkLevel level) {
+        Log.v(TAG, "setDeviceSdkLevel(" + level + ")");
+        switch (level) {
+            case S:
+                // TODO(b/295321663): this is hacky, need to refactor the rule to use SDK ints
+                // directly
+                mockIsAtLeastS(true);
+                mockIsAtLeastSv2(false);
+                mockIsAtLeastT(false);
+                return;
+            case T:
+                mockIsAtLeastS(true);
+                mockIsAtLeastSv2(true);
+                mockIsAtLeastT(true);
+                return;
+            default:
+                throw new UnsupportedOperationException(
+                        "mocking level " + level + " not implemented yet");
+        }
+    }
+}
