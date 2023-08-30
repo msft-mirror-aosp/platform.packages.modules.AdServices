fc71078501c5e17a2a8d395122975cb839182bca
Yang Wang <yangwangyw@google.com>
2023-08-30 13:26:35 +0000

    Revert "Ignore testRecordGaUxNotificationDisplayed test"
    This reverts commit cbb586ec3627c604738e93d9e3bb5eb6edf7d775.
    
    Bug: b/298007005
    
    Reason for revert: unintentional
    
    Change-Id: I1f00ff0b7f22a8f59c9622542c5d2cbf25f90968

--- adservices/tests/unittest/system-service/src/com/android/server/adservices/AdServicesManagerServiceTest.java
+++ adservices/tests/unittest/system-service/src/com/android/server/adservices/AdServicesManagerServiceTest.java
@@ -68,7 +68,6 @@ import com.android.server.adservices.feature.PrivacySandboxUxCollection;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -573,7 +572,6 @@ public class AdServicesManagerServiceTest {
         assertThrows(SecurityException.class, () -> service.getConsent(ConsentParcel.ALL_API));
     }
 
-    @Ignore
     @Test
     public void testRecordGaUxNotificationDisplayed() throws IOException {
         AdServicesManagerService service =
