63ebaed373601e9f42c5732f4de497fa74495434
Aditya Gupta <adigupt@google.com>
2023-09-06 12:06:14 +0000

    Scheduling DebugReportSender job if debug reports are generated after selectAds
    Bug: 299257579
    Test: atest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:82107fe8af222f14e568686fb4b390854651e8d9)
    Merged-In: I0b71efd145ff87f0dd77a421fa6180f68a3f16bb
    Change-Id: I0b71efd145ff87f0dd77a421fa6180f68a3f16bb

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/adselection/DebugReportSenderStrategyBatchImplTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/adselection/DebugReportSenderStrategyBatchImplTest.java
@@ -16,48 +16,80 @@
 
 package com.android.adservices.service.adselection;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
+import android.content.Context;
 import android.net.Uri;
+
+import androidx.test.core.app.ApplicationProvider;
 
 import com.android.adservices.data.adselection.AdSelectionDebugReportDao;
 import com.android.adservices.service.devapi.DevContext;
+import com.android.dx.mockito.inline.extended.ExtendedMockito;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.MockitoSession;
+import org.mockito.quality.Strictness;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class DebugReportSenderStrategyBatchImplTest {
+    private static final Context CONTEXT = ApplicationProvider.getApplicationContext();
     private static final DevContext DEV_CONTEXT = DevContext.createForDevOptionsDisabled();
     @Mock private AdSelectionDebugReportDao mAdSelectionDebugReportDao;
     private DebugReportSenderStrategyBatchImpl mDebugReportSender;
+    private MockitoSession mStaticMockSession;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-
+        ExtendedMockito.doNothing()
+                .when(mAdSelectionDebugReportDao)
+                .persistAdSelectionDebugReporting(anyList());
+        mStaticMockSession =
+                ExtendedMockito.mockitoSession()
+                        .spyStatic(DebugReportSenderJobService.class)
+                        .strictness(Strictness.LENIENT)
+                        .startMocking();
+        ExtendedMockito.doNothing()
+                .when(() -> DebugReportSenderJobService.scheduleIfNeeded(any(), anyBoolean()));
         mDebugReportSender =
-                new DebugReportSenderStrategyBatchImpl(mAdSelectionDebugReportDao, DEV_CONTEXT);
+                new DebugReportSenderStrategyBatchImpl(
+                        CONTEXT, mAdSelectionDebugReportDao, DEV_CONTEXT);
+    }
+
+    @After
+    public void teardown() {
+        if (mStaticMockSession != null) {
+            mStaticMockSession.finishMocking();
+        }
     }
 
     @Test
     public void testConstructorWithInvalidParamsThrowsException() {
         Assert.assertThrows(
                 NullPointerException.class,
-                () -> new DebugReportSenderStrategyBatchImpl(null, DEV_CONTEXT));
+                () ->
+                        new DebugReportSenderStrategyBatchImpl(
+                                null, mAdSelectionDebugReportDao, DEV_CONTEXT));
         Assert.assertThrows(
                 NullPointerException.class,
-                () -> new DebugReportSenderStrategyBatchImpl(mAdSelectionDebugReportDao, null));
+                () -> new DebugReportSenderStrategyBatchImpl(CONTEXT, null, DEV_CONTEXT));
+        Assert.assertThrows(
+                NullPointerException.class,
+                () ->
+                        new DebugReportSenderStrategyBatchImpl(
+                                CONTEXT, mAdSelectionDebugReportDao, null));
     }
 
     @Test
@@ -88,26 +120,57 @@ public class DebugReportSenderStrategyBatchImplTest {
     public void testSend_withSingleReport_allRequestsAreSuccessful()
             throws ExecutionException, InterruptedException {
         Uri uri = Uri.parse("https://example.com/reportWin");
-        doNothing().when(mAdSelectionDebugReportDao).persistAdSelectionDebugReporting(anyList());
-
         mDebugReportSender.enqueue(uri);
-        ListenableFuture<Void> future = mDebugReportSender.flush();
-        future.get();
 
-        verify(mAdSelectionDebugReportDao, times(1)).persistAdSelectionDebugReporting(anyList());
+        mDebugReportSender.flush().get();
+
+        ExtendedMockito.verify(mAdSelectionDebugReportDao, times(1))
+                .persistAdSelectionDebugReporting(anyList());
     }
 
     @Test
     public void testSend_withMultipleReports_allRequestsAreSuccessful()
             throws ExecutionException, InterruptedException {
+
         Uri uri1 = Uri.parse("https://example.com/reportWin");
         Uri uri2 = Uri.parse("https://example.com/reportLoss");
-        doNothing().when(mAdSelectionDebugReportDao).persistAdSelectionDebugReporting(anyList());
-
         mDebugReportSender.batchEnqueue(List.of(uri1, uri2));
-        ListenableFuture<Void> future = mDebugReportSender.flush();
-        future.get();
 
-        verify(mAdSelectionDebugReportDao, times(1)).persistAdSelectionDebugReporting(anyList());
+        mDebugReportSender.flush().get();
+
+        ExtendedMockito.verify(mAdSelectionDebugReportDao, times(1))
+                .persistAdSelectionDebugReporting(anyList());
+    }
+
+    @Test
+    public void testSend_withMultipleReports_schedulesSenderJob()
+            throws ExecutionException, InterruptedException {
+        Uri uri1 = Uri.parse("https://example.com/reportWin");
+        Uri uri2 = Uri.parse("https://example.com/reportLoss");
+        mDebugReportSender.batchEnqueue(List.of(uri1, uri2));
+
+        mDebugReportSender.flush().get();
+
+        ExtendedMockito.verify(
+                () ->
+                        DebugReportSenderJobService.scheduleIfNeeded(
+                                any(Context.class), anyBoolean()),
+                times(1));
+    }
+
+    @Test
+    public void testSend_withNoReports_doesNotScheduleJob()
+            throws ExecutionException, InterruptedException {
+        mDebugReportSender.batchEnqueue(List.of());
+
+        mDebugReportSender.flush().get();
+
+        ExtendedMockito.verify(mAdSelectionDebugReportDao, never())
+                .persistAdSelectionDebugReporting(anyList());
+        ExtendedMockito.verify(
+                () ->
+                        DebugReportSenderJobService.scheduleIfNeeded(
+                                any(Context.class), anyBoolean()),
+                never());
     }
 }
