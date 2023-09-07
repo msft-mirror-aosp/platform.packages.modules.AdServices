e31d6996d0c437f750bb5eb203ef10426666e139
Aditya Gupta <adigupt@google.com>
2023-09-07 13:23:23 +0000

    fixing timeout issue with isLimitedAdTrackingEnabled method
    Bug:298975714
    Test: atest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d0c1aaca72256cb26cdceed35fe026117e4db55a)
    Merged-In: If5dcdcdbbfad77c3489564f5751b368ef0820ab9
    Change-Id: If5dcdcdbbfad77c3489564f5751b368ef0820ab9

--- adservices/service-core/java/com/android/adservices/service/adselection/AdIdFetcher.java
+++ adservices/service-core/java/com/android/adservices/service/adselection/AdIdFetcher.java
@@ -18,31 +18,36 @@ package com.android.adservices.service.adselection;
 
 import static android.adservices.adid.AdId.ZERO_OUT;
 
+import static androidx.concurrent.futures.CallbackToFutureAdapter.Resolver;
+
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 import android.adservices.adid.GetAdIdResult;
 import android.adservices.adid.IGetAdIdCallback;
 import android.annotation.NonNull;
 
-import androidx.concurrent.futures.CallbackToFutureAdapter;
-
 import com.android.adservices.LoggerFactory;
 import com.android.adservices.service.adid.AdIdWorker;
+import com.android.internal.annotations.VisibleForTesting;
 
 import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
 
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /** Class to fetch Ad Id related data required to run ad selection. */
 public class AdIdFetcher {
+    @VisibleForTesting public static final int AD_ID_TIMEOUT_IN_MS = 50;
     private static final boolean DEFAULT_IS_LAT_ENABLED = true;
-    private static final int AD_ID_TIMEOUT_IN_MS = 50;
     private static final LoggerFactory.Logger sLogger = LoggerFactory.getFledgeLogger();
     @NonNull private final AdIdWorker mAdIdWorker;
     @NonNull private final ListeningExecutorService mLightweightExecutorService;
@@ -79,41 +84,12 @@ public class AdIdFetcher {
      */
     public ListenableFuture<Boolean> isLimitedAdTrackingEnabled(
             @NonNull String packageName, int callingUid) {
-        ListenableFuture<Boolean> getAdIdFuture =
-                CallbackToFutureAdapter.getFuture(
-                        completer -> {
-                            mAdIdWorker.getAdId(
-                                    packageName,
-                                    callingUid,
-                                    new IGetAdIdCallback.Stub() {
-                                        @Override
-                                        public void onResult(GetAdIdResult getAdIdResult) {
-                                            String id = getAdIdResult.getAdId();
-                                            boolean isLatEnabledResult =
-                                                    getAdIdResult.isLatEnabled();
-                                            boolean isAdIdZeroedOut = ZERO_OUT.equals(id);
-                                            sLogger.v(
-                                                    "isLatEnabled is: %b.",
-                                                    isLatEnabledResult || isAdIdZeroedOut);
-                                            completer.set(isLatEnabledResult || isAdIdZeroedOut);
-                                        }
-
-                                        @Override
-                                        public void onError(int i) {
-                                            sLogger.v(
-                                                    "Got error calling getAdId API, throwing"
-                                                            + " IllegalStateException");
-                                            completer.setException(
-                                                    new IllegalStateException(
-                                                            "Failed while trying to call getAdId"));
-                                        }
-                                    });
-                            return "isLimitedAdTrackingEnabled"; // Returning the method name for
-                            // better
-                            // tracking
-                        });
-        return FluentFuture.from(getAdIdFuture)
-                .withTimeout(AD_ID_TIMEOUT_IN_MS, MILLISECONDS, mScheduledExecutor)
+        return FluentFuture.from(
+                        getFutureWithTimeout(
+                                convertToCallback(packageName, callingUid),
+                                AD_ID_TIMEOUT_IN_MS,
+                                MILLISECONDS,
+                                mScheduledExecutor))
                 .catching(
                         TimeoutException.class,
                         timeoutException -> {
@@ -132,5 +108,51 @@ public class AdIdFetcher {
                             return DEFAULT_IS_LAT_ENABLED;
                         },
                         mLightweightExecutorService);
+    }
+
+    /**
+     * @return a future with timeout after converting the callback to future
+     */
+    private <T> ListenableFuture<T> getFutureWithTimeout(
+            Resolver<T> resolver,
+            long timeoutDuration,
+            TimeUnit timeoutUnit,
+            ScheduledExecutorService scheduledExecutorService) {
+        SettableFuture<T> out = SettableFuture.create();
+        // Start timeout before invoking the callback
+        ListenableFuture<T> result =
+                Futures.withTimeout(out, timeoutDuration, timeoutUnit, scheduledExecutorService);
+        out.setFuture(androidx.concurrent.futures.CallbackToFutureAdapter.getFuture(resolver));
+        return result;
+    }
+
+    private Resolver<Boolean> convertToCallback(String packageName, int callingUid) {
+        return completer -> {
+            mAdIdWorker.getAdId(
+                    packageName,
+                    callingUid,
+                    new IGetAdIdCallback.Stub() {
+                        @Override
+                        public void onResult(GetAdIdResult getAdIdResult) {
+                            String id = getAdIdResult.getAdId();
+                            boolean isLatEnabledResult = getAdIdResult.isLatEnabled();
+                            boolean isAdIdZeroedOut = ZERO_OUT.equals(id);
+                            sLogger.v(
+                                    "isLatEnabled is: %b.", isLatEnabledResult || isAdIdZeroedOut);
+                            completer.set(isLatEnabledResult || isAdIdZeroedOut);
+                        }
+
+                        @Override
+                        public void onError(int i) {
+                            sLogger.v(
+                                    "Got error calling getAdId API, throwing"
+                                            + " IllegalStateException");
+                            completer.setException(
+                                    new IllegalStateException(
+                                            "Failed while trying to call getAdId"));
+                        }
+                    });
+            return "isLimitedAdTrackingEnabled";
+        };
     }
 }
