1d8c59b58c54ecba10a1cfd299be9975501a7474
Robert Marcus <ramarcus@google.com>
2023-10-06 20:38:33 +0000

    Remove usages of CompatAdServicesTestUtils from permissions tests
    Test: atest com.android.adservices.tests.permissions
    Bug: 299104530
    Change-Id: I8da9cb0967c5bfa1555bcfba9d2456b446250125

--- adservices/tests/test-util/side-less/com/android/adservices/common/AbstractAdServicesFlagsSetterRule.java
+++ adservices/tests/test-util/side-less/com/android/adservices/common/AbstractAdServicesFlagsSetterRule.java
@@ -362,8 +362,19 @@ abstract class AbstractAdServicesFlagsSetterRule<T extends AbstractAdServicesFla
     /**
      * Overrides flag used by {@link com.android.adservices.service.PhFlags#getPpapiAppAllowList()}.
      */
+    // <p> TODO (b/303901926) - apploy consistent naming to allow list methods
     public T setPpapiAppAllowList(String value) {
         return setOrCacheFlag(FlagsConstants.KEY_PPAPI_APP_ALLOW_LIST, value, ALLOWLIST_SEPARATOR);
+    }
+
+    /**
+     * Overrides flag used by (@link
+     * com.android.adservices.service.PhFlags#getPpapiAppSignatureAllowList()}. NOTE: this will
+     * completely override the allow list, *not* append to it.
+     */
+    // <p> TODO (b/303901926) - apply consistent naming to allow list methods
+    public T overridePpapiAppSignatureAllowList(String value) {
+        return setFlag(FlagsConstants.KEY_PPAPI_APP_SIGNATURE_ALLOW_LIST, value);
     }
 
     /**
