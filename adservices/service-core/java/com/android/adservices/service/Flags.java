090974e2c6e7256e99848cc659767d081bd3775e
Parag Kulkarni <paragkulkarni@google.com>
2023-10-18 02:15:11 +0000

    Update the MobileDataDownloadTest to read the latest enrollment data and update Enrollment URL flag to point to new default file.
    Bug: 306058451
    Test: atest AdServicesMddIntegrationTests AdServicesServiceCoreUnitTests
    Change-Id: I38c6cfd60aacdac2f6c6ff2bb55f642048cc400d

--- adservices/service-core/java/com/android/adservices/service/Flags.java
+++ adservices/service-core/java/com/android/adservices/service/Flags.java
@@ -353,8 +353,8 @@ public interface Flags {
 
     /** Measurement manifest file url, used for MDD download. */
     String MEASUREMENT_MANIFEST_FILE_URL =
-            "https://www.gstatic.com/mdi-serving/rubidium-adservices-adtech-enrollment/2324"
-                    + "/3927729583a9dbfdb9a3eaa84ddcef3d9b46c3c7";
+            "https://www.gstatic.com/mdi-serving/rubidium-adservices-adtech-enrollment/2483"
+                    + "/99f68a201189da021b1f3dd4ebdef7b0fbe75892";
 
     /** Measurement manifest file url. */
     default String getMeasurementManifestFileUrl() {
