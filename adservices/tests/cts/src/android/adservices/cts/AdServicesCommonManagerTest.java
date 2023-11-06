372ae91acbdcc0f28048ae6f9280be236f2afdf1
hanlixy <hanlixy@google.com>
2023-11-06 22:00:51 +0000

    Remove UpdateAdId APIs in mainline-adservices-release
    This CL should be only submitted in branch mainline-adservices-release， , as this release doesn't allow API changes.
    
    Test: Pre-submit
    Bug: 309659592
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:cb1e23ece0c609dbf883ef60dfdacb242ad90594)
    Merged-In: Id1ca600f10a14ed93fee19311a3b2e9ed1f19594
    Change-Id: Id1ca600f10a14ed93fee19311a3b2e9ed1f19594

--- adservices/tests/cts/src/android/adservices/cts/AdServicesCommonManagerTest.java
+++ adservices/tests/cts/src/android/adservices/cts/AdServicesCommonManagerTest.java
@@ -20,10 +20,8 @@ import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assert.assertThrows;
 
-import android.adservices.adid.AdId;
 import android.adservices.common.AdServicesCommonManager;
 import android.adservices.common.AdServicesOutcomeReceiver;
-import android.adservices.common.UpdateAdIdRequest;
 import android.content.Context;
 import android.os.OutcomeReceiver;
 
@@ -32,7 +30,6 @@ import androidx.test.core.app.ApplicationProvider;
 
 import com.android.adservices.common.AdServicesDeviceSupportedRule;
 import com.android.adservices.common.AdServicesFlagsSetterRule;
-import com.android.adservices.common.OutcomeReceiverForTests;
 import com.android.adservices.common.RequiresSdkLevelAtLeastS;
 import com.android.adservices.common.SdkLevelSupportRule;
 
@@ -128,62 +125,6 @@ public class AdServicesCommonManagerTest {
                 .isInstanceOf(SecurityException.class);
     }
 
-    @Test
-    @RequiresSdkLevelAtLeastS(reason = "uses OutcomeReceiver, which is only available on T")
-    public void tesUpdateAdIdCache_notAuthorized_sPlus() throws Exception {
-        flags.setUpdateAdIdCacheEnabled(true);
-
-        UpdateAdIdRequest request = new UpdateAdIdRequest.Builder(AdId.ZERO_OUT).build();
-
-        OutcomeReceiverForTests<Boolean> updateAdIdCallback = new OutcomeReceiverForTests<>();
-        mCommonManager.updateAdId(request, CALLBACK_EXECUTOR, updateAdIdCallback);
-
-        updateAdIdCallback.assertFailure(SecurityException.class);
-    }
-
-    @Test
-    @RequiresSdkLevelAtLeastS(reason = "uses OutcomeReceiver, which is only available on T")
-    public void tesUpdateAdIdCache_notEnabled_sPlus() throws Exception {
-        flags.setUpdateAdIdCacheEnabled(false);
-
-        UpdateAdIdRequest request = new UpdateAdIdRequest.Builder(AdId.ZERO_OUT).build();
-
-        OutcomeReceiverForTests<Boolean> updateAdIdCallback = new OutcomeReceiverForTests<>();
-        mCommonManager.updateAdId(request, CALLBACK_EXECUTOR, updateAdIdCallback);
-
-        updateAdIdCallback.assertFailure(IllegalStateException.class);
-    }
-
-    @Test
-    public void tesUpdateAdIdCache_notAuthorized_rPlus() {
-        flags.setUpdateAdIdCacheEnabled(true);
-
-        ListenableFuture<Boolean> updateAdIdCacheResponse = updateAdIdCacheRMinus();
-
-        Exception updateAdIdResponseException =
-                assertThrows(
-                        ExecutionException.class,
-                        () -> updateAdIdCacheResponse.get(1, TimeUnit.SECONDS));
-        assertThat(updateAdIdResponseException)
-                .hasCauseThat()
-                .isInstanceOf(SecurityException.class);
-    }
-
-    @Test
-    public void tesUpdateAdIdCache_notEnabled_rPlus() {
-        flags.setUpdateAdIdCacheEnabled(false);
-
-        ListenableFuture<Boolean> updateAdIdCacheResponse = updateAdIdCacheRMinus();
-
-        Exception updateAdIdResponseException =
-                assertThrows(
-                        ExecutionException.class,
-                        () -> updateAdIdCacheResponse.get(1, TimeUnit.SECONDS));
-        assertThat(updateAdIdResponseException)
-                .hasCauseThat()
-                .isInstanceOf(IllegalStateException.class);
-    }
-
     private ListenableFuture<Boolean> getAdservicesStatus() {
         return CallbackToFutureAdapter.getFuture(
                 completer -> {
@@ -210,29 +151,6 @@ public class AdServicesCommonManagerTest {
         return CallbackToFutureAdapter.getFuture(
                 completer -> {
                     mCommonManager.isAdServicesEnabled(
-                            CALLBACK_EXECUTOR,
-                            new AdServicesOutcomeReceiver<>() {
-                                @Override
-                                public void onResult(Boolean result) {
-                                    completer.set(result);
-                                }
-
-                                @Override
-                                public void onError(Exception error) {
-                                    completer.setException(error);
-                                }
-                            });
-                    // This value is used only for debug purposes: it will be used in toString()
-                    // of returned future or error cases.
-                    return "getStatus";
-                });
-    }
-
-    private ListenableFuture<Boolean> updateAdIdCacheRMinus() {
-        return CallbackToFutureAdapter.getFuture(
-                completer -> {
-                    mCommonManager.updateAdId(
-                            new UpdateAdIdRequest.Builder(AdId.ZERO_OUT).build(),
                             CALLBACK_EXECUTOR,
                             new AdServicesOutcomeReceiver<>() {
                                 @Override
