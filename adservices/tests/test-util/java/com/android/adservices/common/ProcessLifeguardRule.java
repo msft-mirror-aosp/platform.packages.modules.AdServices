a4d1a48cac95046b8087f61e38d8e7962864d3ae
Felipe Leme <felipeal@google.com>
2023-11-28 03:48:31 +0000

    Improved ProcessLifeGuardTest so it prints which tests ran so far.
    Test:  m -j AdServicesTestUtilityTests && adb install -t -g $OUT/testcases/AdServicesTestUtilityTests/*/AdServicesTestUtilityTests.apk && adb shell am instrument -w --no-hidden-api-checks -e class com.android.adservices.common.ProcessLifeguardTestSuite -w com.android.adservices.testutil.tests/androidx.test.runner.AndroidJUnitRunner
    Test: manual verification forcing an error
    Test: atest  --iterations 10 AdServicesServiceCoreUnitTests
    
    Bug: 303112789
    Bug: 313554699
    
    Change-Id: I109f07f8b2558b6c243407074eaa62c52291713f

--- adservices/tests/test-util/java/com/android/adservices/common/ProcessLifeguardRule.java
+++ adservices/tests/test-util/java/com/android/adservices/common/ProcessLifeguardRule.java
@@ -17,6 +17,13 @@ package com.android.adservices.common;
 
 import android.os.Looper;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Rule used to protect the test process from crashing if an uncaught exception is thrown in the
  * background.
@@ -34,5 +41,79 @@ public final class ProcessLifeguardRule extends AbstractProcessLifeguardRule {
     @Override
     protected boolean isMainThread() {
         return Looper.getMainLooper().isCurrentThread();
+    }
+
+    @Override
+    protected UncaughtBackgroundException newUncaughtBackgroundException(
+            String testName,
+            List<String> allTests,
+            List<String> lastTests,
+            Throwable uncaughtThrowable) {
+        saveToSdCard(testName, allTests, lastTests, /* testFailure= */ null, uncaughtThrowable);
+        return super.newUncaughtBackgroundException(
+                testName, allTests, lastTests, uncaughtThrowable);
+    }
+
+    @Override
+    protected UncaughtBackgroundException newUncaughtBackgroundException(
+            String testName,
+            List<String> allTests,
+            List<String> lastTests,
+            Throwable testFailure,
+            Throwable uncaughtThrowable) {
+        saveToSdCard(testName, allTests, lastTests, testFailure, uncaughtThrowable);
+        return super.newUncaughtBackgroundException(
+                testName, allTests, lastTests, testFailure, uncaughtThrowable);
+    }
+
+    private void saveToSdCard(
+            String testName,
+            List<String> allTests,
+            List<String> lastTests,
+            @Nullable Throwable testFailure,
+            Throwable uncaughtThrowable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+
+        sw.append("Uncaught failure: ");
+        uncaughtThrowable.printStackTrace(pw);
+
+        if (testFailure != null) {
+            sw.append("Test failure: ");
+            testFailure.printStackTrace(pw);
+        }
+
+        sw.append("" + allTests.size()).append(" total tests:\n");
+        allTests.forEach(t -> sw.append('\t').append(t).append('\n'));
+        sw.append("" + lastTests.size()).append(" tests since last failure:\n");
+        lastTests.forEach(t -> sw.append('\t').append(t).append('\n'));
+
+        // TODO(b/303112789): un-hardcode /Documents
+        writeFile(
+                "Documents",
+                getClass().getSimpleName()
+                        + "-"
+                        + testName
+                        + "-"
+                        + System.currentTimeMillis()
+                        + ".txt",
+                sw.toString());
+    }
+
+    // TODO(b/303112789): move to common class
+    private void writeFile(String directory, String filename, String contents) {
+        // TODO(b/303112789): un-hardcode /sdcard
+        String path = "/sdcard/" + directory;
+        Path filePath = Paths.get(path, filename);
+        try {
+            mLog.i("Creating file %s", filePath);
+            Files.createFile(filePath);
+            byte[] bytes = contents.getBytes();
+            mLog.d("Writing %d bytes to %s", bytes.length, filePath);
+            Files.write(filePath, bytes);
+            mLog.d("Saul Goodman!");
+        } catch (Exception e) {
+            mLog.e(e, "Failed to save %s", filePath);
+        }
     }
 }
