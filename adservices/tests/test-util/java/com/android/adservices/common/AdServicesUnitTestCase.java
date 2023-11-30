787cdb5b6a1648af5cbb972ce6b96db75a61fcb8
Felipe Leme <felipeal@google.com>
2023-11-30 17:03:26 +0000

    Added infra on AdServicesUnitTestCase to emulate background issues.
    Test: adb shell setprop debug.adservices.test.postTestDelay 500 &&\
      adb shell setprop debug.adservices.test.postTestThrownFrequency 2 &&\
      atest AdServicesServiceCoreUnitTests:AppManifestConfigMetricsLoggerTest
    
    Bug: 303112789
    Bug: 313554699
    Bug: 285014040
    
    Change-Id: I847946f547cc9a91197b7b2b5e1976d179171666

--- adservices/tests/test-util/java/com/android/adservices/common/AdServicesUnitTestCase.java
+++ adservices/tests/test-util/java/com/android/adservices/common/AdServicesUnitTestCase.java
@@ -15,11 +15,21 @@
  */
 package com.android.adservices.common;
 
+import android.os.SystemClock;
+import android.os.SystemProperties;
+import android.util.Log;
+
 import com.android.adservices.shared.testing.common.ApplicationContextSingletonRule;
 
 import com.google.common.truth.Expect;
+import com.google.errorprone.annotations.FormatMethod;
+import com.google.errorprone.annotations.FormatString;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
+
+// TODO(b/285014040): need to add unit tests for this class itself, as it's now providing logic.
 
 /**
  * Base class for all unit tests.
@@ -31,6 +41,29 @@ import org.junit.Rule;
  * {@code SdkLevelSupportRule}, as that rule cannot be defined here yet.
  */
 public abstract class AdServicesUnitTestCase {
+
+    private static final String TAG = AdServicesUnitTestCase.class.getSimpleName();
+
+    // NOTE: properties below are meant to be used to debug / reproduce test failures, they should
+    // NOT be used programmatically in the tests themselves.
+
+    /** When set, defines duration (in milliseconds) to sleep after each test. */
+    private static final String PROP_DELAY_AFTER_TEST = "debug.adservices.test.postTestDelay";
+
+    /**
+     * When set to a (non-zero) number, test will throw a RuntimeException in a background time at
+     * the value defined by it, either once (if the number is positive) or repeatedly.
+     *
+     * <p>For example, if the value is {@code 3}, the 3rd test - and only the 3rd test - will throw
+     * the exception after it's done. But if it's {@code -3}, then the exception will be thrown
+     * after the 3rd test, 6th test, etc... .
+     */
+    private static final String PROP_EXCEPTION_THROWN_FREQUENCY =
+            "debug.adservices.test.postTestThrownFrequency";
+
+    private static int sTestCount;
+
+    private int mTestNumber;
 
     protected final String mTag = getClass().getSimpleName();
 
@@ -49,4 +82,107 @@ public abstract class AdServicesUnitTestCase {
 
     @Rule(order = 7)
     public final Expect expect = Expect.create();
+
+    @Before
+    public final void setTestNumber() {
+        mTestNumber = ++sTestCount;
+        Log.d(TAG, "setTestNumber(): " + getTestName() + " is test #" + mTestNumber);
+    }
+
+    @After
+    public final void postTestOptionalActions() {
+        throwExceptionInBgAfterTest();
+        sleepAfterTest();
+    }
+
+    /** Gets the name of the test being executed. */
+    protected final String getTestName() {
+        return processLifeguard.getTestName();
+    }
+
+    /** Sleeps for the given amount of time. */
+    @FormatMethod
+    protected final void sleep(
+            int timeMs, @FormatString String reasonFmt, @Nullable Object... reasonArgs) {
+        String reason = String.format(reasonFmt, reasonArgs);
+        Log.i(
+                TAG,
+                getTestName()
+                        + ": napping "
+                        + timeMs
+                        + "ms on thread "
+                        + Thread.currentThread()
+                        + ". Reason: "
+                        + reason);
+        SystemClock.sleep(timeMs);
+        Log.i(TAG, "Little Suzie woke up!");
+    }
+
+    /**
+     * Throws a runtime exception (with the given {@code message}) in the background.
+     *
+     * <p>By default, it starts a thread (with the given {@code threadName}) that throws right away,
+     * but subclasses could override it to change the behavior (for example, to use an existing
+     * executor).
+     */
+    protected void throwExceptionInBg(String threadName, String message) {
+        RuntimeException exception = new RuntimeException(message);
+        Log.i(TAG, "Starting thread " + threadName + " (which will throw " + exception + ")");
+        new Thread(
+                        () -> {
+                            throw exception;
+                        },
+                        threadName)
+                .start();
+    }
+
+    private void sleepAfterTest() {
+        int napTimeMs = SystemProperties.getInt(PROP_DELAY_AFTER_TEST, 0);
+        if (napTimeMs <= 0) {
+            return;
+        }
+        sleep(
+                napTimeMs,
+                "forcing sleep after test as requested by system property %s",
+                PROP_DELAY_AFTER_TEST);
+    }
+
+    private void throwExceptionInBgAfterTest() {
+        int frequency = SystemProperties.getInt(PROP_EXCEPTION_THROWN_FREQUENCY, 0);
+        if (frequency == 0) {
+            return;
+        }
+
+        boolean throwException =
+                (frequency < 0 && (mTestNumber % frequency != 0))
+                        || (frequency > 0 && mTestNumber == frequency);
+
+        if (!throwException) {
+            Log.i(
+                    TAG,
+                    "Not throwing exception after test #"
+                            + mTestNumber
+                            + " (frequency="
+                            + frequency
+                            + ")");
+            return;
+        }
+
+        Log.e(
+                TAG,
+                "Throwing exception after test #" + mTestNumber + " (frequency=" + frequency + ")");
+
+        String threadName = getTestName() + "-postTest-#" + mTestNumber;
+        String message =
+                getTestName()
+                        + " failing @After "
+                        + mTestNumber
+                        + " invocation(s) of "
+                        + "test methods from classes that extend "
+                        + AdServicesUnitTestCase.class.getSimpleName()
+                        + " (as requested by property "
+                        + PROP_EXCEPTION_THROWN_FREQUENCY
+                        + ")";
+        throwExceptionInBg(threadName, message);
+    }
 }
