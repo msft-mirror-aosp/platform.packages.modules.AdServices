f2a3e48214f65ee0a80e1fc1298269e195e373e2
Will Burr <wgb@google.com>
2024-01-10 18:06:00 +0000

    DO NOT MERGE: Skip CTS tests on SDK sandbox for Go devices
    We should not run CTS tests which involve running a sandbox or
    instrumenting a sandbox on Go devices. We also omit TV and automative
    devices.
    
    Bug: 319399368
    Test: atest --test-mapping
          packages/modules/AdServices/sdksandbox/TEST_MAPPING
    
    Change-Id: I5d3b5a08dfd1ab88c88ed16c1144665b1b77a970

--- sdksandbox/tests/testutils/src/android/app/sdksandbox/testutils/DeviceSupportUtils.java
+++ sdksandbox/tests/testutils/src/android/app/sdksandbox/testutils/DeviceSupportUtils.java
@@ -17,16 +17,47 @@
 package android.app.sdksandbox.testutils;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
+
+import com.android.compatibility.common.util.FeatureUtil;
+import com.android.compatibility.common.util.PackageUtil;
+import com.android.compatibility.common.util.PropertyUtil;
 
 /** Utility class to control which devices SDK sandbox tests run on. */
 public class DeviceSupportUtils {
 
+    private static final String GMS_CORE_PACKAGE = "com.google.android.gms";
+    private static final String PLAY_STORE_PACKAGE = "com.android.vending";
+    private static final String PLAY_STORE_USER_CERT =
+            "F0:FD:6C:5B:41:0F:25:CB:25:C3:B5:33:46:C8:97:2F:"
+                    + "AE:30:F8:EE:74:11:DF:91:04:80:AD:6B:2D:60:DB:83";
+
     public static boolean isSdkSandboxSupported(Context context) {
-        return !isWatch(context);
+        return !FeatureUtil.isWatch()
+                && !FeatureUtil.isTV()
+                && !FeatureUtil.isAutomotive()
+                && !isGoDevice();
     }
 
-    private static boolean isWatch(Context context) {
-        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WATCH);
+    // Taken from vendor/xts/common/device-side/util/src/com/android/xts/common/util/GmsUtil.java
+    private static boolean isGoDevice() {
+        return FeatureUtil.isLowRam()
+                && hasGmsCore()
+                && hasPlayStore()
+                && !FeatureUtil.isWatch()
+                && !FeatureUtil.isTV()
+                && !FeatureUtil.isAutomotive()
+                && !FeatureUtil.isVrHeadset()
+                && !FeatureUtil.isArc();
+    }
+
+    /** Returns whether GMS Core is installed for this build */
+    private static boolean hasGmsCore() {
+        return PackageUtil.exists(GMS_CORE_PACKAGE);
+    }
+
+    private static boolean hasPlayStore() {
+        return (PropertyUtil.isUserBuild())
+                ? PackageUtil.exists(PLAY_STORE_PACKAGE, PLAY_STORE_USER_CERT)
+                : PackageUtil.exists(PLAY_STORE_PACKAGE);
     }
 }
