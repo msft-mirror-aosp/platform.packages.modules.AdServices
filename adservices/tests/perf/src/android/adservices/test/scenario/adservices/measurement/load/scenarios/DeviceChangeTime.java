4e1f1b1e42efd4970d52a7ea08f2505a996949f9
cbail <cbail@google.com>
2024-01-17 12:23:30 -0600

    Measurement Load Testing Framework
    MVP for Measurement Load Testing Framework. The section allows change adds the base framework that can be extended to meet various load testing needs.
    
    Bug: 296423412
    Test: go/rb-msmt-load
    Change-Id: I95483bab0bd14bc4c5e89386c4f52973f9c0a9c1

---
+++ adservices/tests/perf/src/android/adservices/test/scenario/adservices/measurement/load/scenarios/DeviceChangeTime.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.adservices.test.scenario.adservices.measurement.load.scenarios;
+
+import android.platform.test.option.StringOption;
+import android.platform.test.scenario.annotation.Scenario;
+import android.util.Log;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@Scenario
+@RunWith(JUnit4.class)
+public class DeviceChangeTime extends AbstractTestAction {
+
+    private static final String OPTION_DATE = "date";
+
+    @Rule public final StringOption dateOption = new StringOption(OPTION_DATE);
+
+    @Test
+    public void setDate() {
+        InstrumentationRegistry.getInstrumentation()
+                .getUiAutomation()
+                .executeShellCommand("date %s".formatted(dateOption.get()));
+        Log.i(TAG, generateLog("Date Set : %s".formatted(dateOption.get())));
+    }
+}
