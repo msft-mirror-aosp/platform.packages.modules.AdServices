7a46b9d42a2ea8ea54a44326c1e187f6a5360388
Priyank Devpura <pdevpura@google.com>
2024-01-29 18:26:02 +0000

    Revert^2 "Delegate adservices shell command from sdksandbox to adservices manager."
    This reverts commit 8f51602756dad54d1d750d9a199cba1c2f3f3841.
    
    Test: atest SdkSandboxManagerServiceUnitTest, SdkSandboxShellCommandUnitTest
    
    Test: adb shell device_config put adservices adservices_shell_command_enabled true && adb shell cmd adservices_manager
          adb shell cmd sdk_sandbox adservices echo hello
    
    Bug: 322822651
    Bug: 308009734
    
    Reason for revert: Test was failing when it was creating dup filedescriptor. This seems like a temporary error. The code seems to work in Prod. Will mark the test as flaky to view if the test was failing was conincidence.
    
    Change-Id: Ie8c59530722762e9019ae8c568da7b41bf94677d

--- sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
+++ sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
@@ -272,8 +272,10 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
         }
 
         SdkSandboxShellCommand createShellCommand(
-                SdkSandboxManagerService service, Context context) {
-            return new SdkSandboxShellCommand(service, context);
+                SdkSandboxManagerService service,
+                Context context,
+                boolean supportsAdServicesShellCmd) {
+            return new SdkSandboxShellCommand(service, context, supportsAdServicesShellCmd);
         }
 
         boolean isEmulator() {
@@ -1732,8 +1734,9 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
     @Override
     public int handleShellCommand(ParcelFileDescriptor in, ParcelFileDescriptor out,
             ParcelFileDescriptor err, String[] args) {
+        boolean supportsAdServicesShellCmd = !mAdServicesManagerPublished;
         return mInjector
-                .createShellCommand(this, mContext)
+                .createShellCommand(this, mContext, supportsAdServicesShellCmd)
                 .exec(
                         this,
                         in.getFileDescriptor(),
