d912a97cdd0c3ff8cec8dcd51102a1786bf6b96b
Ben Nissan <bennissan@google.com>
2024-02-09 22:53:36 +0000

    Add Custom Audience host test and sample apps
    This CL adds a host test to automate Custom Audience end-to-end test
    scenarios, with sample apps to be used within the test.
    
    Bug: 322225316
    Test: atest CustomAudienceTest
    Change-Id: Ia0af6a8c8ff5c747c8be03a882b418f116dc2830

---
+++ adservices/tests/cts/endtoends/customaudience/src/com/android/adservices/tests/cts/customaudience/CtsAdServicesCustomAudienceTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.tests.cts.customaudience;
+
+import static com.android.adservices.common.AdServicesCtsTestCase.LOGCAT_TAG_FLEDGE;
+import static com.android.adservices.service.CommonFlagsConstants.KEY_ADSERVICES_SYSTEM_SERVICE_ENABLED;
+import static com.android.adservices.service.FlagsConstants.KEY_CONSENT_MANAGER_DEBUG_MODE;
+import static com.android.adservices.service.FlagsConstants.KEY_CONSENT_NOTIFIED_DEBUG_MODE;
+import static com.android.adservices.service.FlagsConstants.KEY_DISABLE_FLEDGE_ENROLLMENT_CHECK;
+import static com.android.adservices.service.FlagsConstants.KEY_ENFORCE_FOREGROUND_STATUS_FLEDGE_CUSTOM_AUDIENCE;
+import static com.android.adservices.service.FlagsConstants.KEY_ENFORCE_FOREGROUND_STATUS_FLEDGE_RUN_AD_SELECTION;
+import static com.android.adservices.service.FlagsConstants.KEY_FLEDGE_CUSTOM_AUDIENCE_SERVICE_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_FLEDGE_SELECT_ADS_KILL_SWITCH;
+
+import com.android.adservices.common.AdServicesCtsTestCase;
+import com.android.adservices.common.AdServicesFlagsSetterRule;
+import com.android.adservices.shared.testing.annotations.EnableDebugFlag;
+import com.android.adservices.shared.testing.annotations.SetFlagDisabled;
+import com.android.adservices.shared.testing.annotations.SetFlagEnabled;
+import com.android.adservices.shared.testing.annotations.SetLogcatTag;
+
+@SetFlagDisabled(KEY_FLEDGE_CUSTOM_AUDIENCE_SERVICE_KILL_SWITCH)
+@SetFlagDisabled(KEY_FLEDGE_CUSTOM_AUDIENCE_SERVICE_KILL_SWITCH)
+@SetFlagDisabled(KEY_FLEDGE_SELECT_ADS_KILL_SWITCH)
+@SetFlagEnabled(KEY_ADSERVICES_SYSTEM_SERVICE_ENABLED)
+@SetFlagDisabled(KEY_ENFORCE_FOREGROUND_STATUS_FLEDGE_RUN_AD_SELECTION)
+@SetFlagDisabled(KEY_ENFORCE_FOREGROUND_STATUS_FLEDGE_CUSTOM_AUDIENCE)
+@EnableDebugFlag(KEY_CONSENT_NOTIFIED_DEBUG_MODE)
+@EnableDebugFlag(KEY_CONSENT_MANAGER_DEBUG_MODE)
+@EnableDebugFlag(KEY_DISABLE_FLEDGE_ENROLLMENT_CHECK)
+@SetLogcatTag(tag = LOGCAT_TAG_FLEDGE)
+public abstract class CtsAdServicesCustomAudienceTestCase extends AdServicesCtsTestCase {
+    // As adb commands, app installs/uninstalls, and broadcast processing require time to execute,
+    // add this waiting time to give them enough time to execute. This helps reduce test flakiness.
+    protected static final long EXECUTION_WAITING_TIME = 10_000L;
+
+    @Override
+    protected AdServicesFlagsSetterRule getAdServicesFlagsSetterRule() {
+        return AdServicesFlagsSetterRule.forGlobalKillSwitchDisabledTests().setCompatModeFlags();
+    }
+}
