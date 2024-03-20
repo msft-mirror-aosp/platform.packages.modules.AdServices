bd65d60bd7ce95833fb97140b1ed1dfce088ea36
xufan <xufan@google.com>
2024-03-20 01:09:41 +0000

    [PAS] Adding PAS telemetry stats infra.
    New:
    1) EncodingJobRunStats
    2) EncodingJsExecutionStats
    3) EncodingJsFetchStats
    4) PersistAdSelectionResultCalledStats
    5) UpdateSignalsApiCalledStats
    
    Bug: 323409733
    Bug: 329124976
    Change-Id: Ida7cac1bfef2a1961d7c71db6576763d6770f454

---
+++ adservices/service-core/java/com/android/adservices/service/stats/pas/EncodingJsFetchStats.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.service.stats.pas;
+
+import android.adservices.common.AdsRelevanceStatusUtils;
+
+import com.google.auto.value.AutoValue;
+
+/** Class for logging per download of the encoding javascript stats. */
+@AutoValue
+public abstract class EncodingJsFetchStats {
+    /** Returns the time to download the js. */
+    public abstract int getJsDownloadTime();
+
+    /** Returns http response code. */
+    public abstract int getHttpResponseCode();
+
+    /** Returns the status of JavaScript fetch. */
+    @AdsRelevanceStatusUtils.JsFetchStatus
+    public abstract int getFetchStatus();
+
+    /** Returns AdTech's eTLD+1 when the JsFetchStatus is not success. */
+    public abstract String getAdTechId();
+
+    /** Returns generic builder */
+    public static Builder builder() {
+        return new AutoValue_EncodingJsFetchStats.Builder();
+    }
+
+    /** Builder class for EncodingJsFetchStats. */
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder setJsDownloadTime(int value);
+
+        public abstract Builder setHttpResponseCode(int value);
+
+        public abstract Builder setFetchStatus(
+            @AdsRelevanceStatusUtils.JsFetchStatus int value);
+
+        public abstract Builder setAdTechId(String value);
+
+        public abstract EncodingJsFetchStats build();
+    }
+}
