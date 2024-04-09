e9d710e8b427f5bc91cf29b9df27d9045eff6b0e
Rishabh Goel <ardroid@google.com>
2024-04-09 16:05:40 +0000

    Create separate class (DebugFlags) for debug flags
    Bug: 330796095
    Test: atest DebugFlagsTest
    Change-Id: I5c30d0028f1c4fef24779f17aeab81d6e3097c92

---
+++ adservices/service-core/java/com/android/adservices/service/DebugFlags.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.service;
+
+import static com.android.adservices.service.DebugFlagsConstants.KEY_CONSENT_MANAGER_OTA_DEBUG_MODE;
+import static com.android.adservices.service.Flags.DEFAULT_CONSENT_MANAGER_OTA_DEBUG_MODE;
+import static com.android.adservices.service.PhFlags.getSystemPropertyName;
+
+import android.os.SystemProperties;
+
+/** Debug Flags Implementation that delegates to System Properties. */
+public final class DebugFlags {
+    private static final DebugFlags sInstance = new DebugFlags();
+
+    static DebugFlags getInstance() {
+        return sInstance;
+    }
+
+    private DebugFlags() {}
+
+    public boolean getConsentManagerOTADebugMode() {
+        return getDebugFlag(
+                KEY_CONSENT_MANAGER_OTA_DEBUG_MODE, DEFAULT_CONSENT_MANAGER_OTA_DEBUG_MODE);
+    }
+
+    private boolean getDebugFlag(String name, boolean defaultValue) {
+        return SystemProperties.getBoolean(getSystemPropertyName(name), defaultValue);
+    }
+}
