74a5e8c66b5ff584ed4ae182a8996cfd24633492
Akhil Gangu <akhilga@google.com>
2024-04-24 21:58:50 +0000

    Introduce generic log verification framework for supporting ErrorLogUtil calls.
    We should be able to easily configure new log types in AdServicesLoggingUsageRule. This CL introduces a generic design to accomplish that and uses it to support verification of ErrorLogUtil calls. Goal of the CL is to support explicit verification of both ErrorLogUtils.e(Throwable, int, int) and ErrrorLogUtils.e(int, int) calls in unit tests. Multiple annotation support and other features to simplify log verification in tests will be implemented in other CLs.
    
    Bug: 337042949
    Test: atest AdServicesSharedLibrariesUnitTests AdServicesTestUtilityTests
    Flag: TEST_ONLY
    Change-Id: I66b35b71b41e0412faaaf351b1f32607c1dd9172

---
+++ adservices/tests/test-util/java/com/android/adservices/common/logging/ErrorLogUtilCall.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.common.logging;
+
+import com.android.adservices.common.logging.annotations.ExpectErrorLogUtilCall;
+import com.android.adservices.shared.testing.LogCall;
+
+import java.util.Objects;
+
+/** Corresponding class for {@link ExpectErrorLogUtilCall}. */
+public final class ErrorLogUtilCall extends LogCall {
+    public final Class<? extends Throwable> mThrowable;
+    public final int mErrorCode;
+    public final int mPpapiName;
+
+    /** Init ErrorLogUtilCall with default number of times (i.e. 1). */
+    public ErrorLogUtilCall(Class<? extends Throwable> throwable, int errorCode, int ppapiName) {
+        this(throwable, errorCode, ppapiName, ExpectErrorLogUtilCall.DEFAULT_TIMES);
+    }
+
+    /** Init ErrorLogUtilCall with specific number of times. */
+    public ErrorLogUtilCall(
+            Class<? extends Throwable> throwable, int errorCode, int ppapiName, int times) {
+        super(times);
+        mThrowable = throwable;
+        mErrorCode = errorCode;
+        mPpapiName = ppapiName;
+    }
+
+    @Override
+    public boolean isIdenticalInvocation(LogCall o) {
+        if (!(o instanceof ErrorLogUtilCall other)) {
+            return false;
+        }
+
+        return Objects.equals(mThrowable, other.mThrowable)
+                && this.mErrorCode == other.mErrorCode
+                && this.mPpapiName == other.mPpapiName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mThrowable, mErrorCode, mPpapiName);
+    }
+}
--- shared/libraries/device-side/java/com/android/adservices/shared/testing/AbstractLogVerifier.java
+++ shared/libraries/device-side/java/com/android/adservices/shared/testing/AbstractLogVerifier.java
@@ -16,12 +16,30 @@
 
 package com.android.adservices.shared.testing;
 
+import android.util.Log;
+
 import org.junit.runner.Description;
 
-/** Abstract log verifier to hold common logic across all log verifiers. */
-// TODO (b/323000746): Design for log verifiers subjected to
-//  change. This was only created to abstract out logic from AdServicesLoggingUsageRule.
-public abstract class AbstractLogVerifier implements LogVerifier {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Abstract log verifier to hold common logic across all log verifiers.
+ *
+ * @param <T> expected {@link LogCall} type to be verified
+ */
+// TODO (b/323000746) - Add unit test coverage for this file after call comparison logic has been
+//  implemented.
+public abstract class AbstractLogVerifier<T extends LogCall> implements LogVerifier {
+    protected final String mTag = getClass().getSimpleName();
+
+    // Key is the LogCall object, value is the source of truth for times; we do not keep track of
+    // the times value in the LogCall object itself because we would like to group together all
+    // identical log call invocations, irrespective of number of times they have been invoked.
+    private final Map<T, Integer> mActualCalls = new HashMap<>();
+
     @Override
     public void setup() {
         mockLogCalls();
@@ -29,9 +47,55 @@ public abstract class AbstractLogVerifier implements LogVerifier {
 
     @Override
     public void verify(Description description) {
-        throw new UnsupportedOperationException("TODO");
+        // Obtain expected log call entries from subclass.
+        Set<T> expectedCalls = getExpectedLogCalls(description);
+        // Extract actual log calls entries from map that was built through mocking log calls.
+        Set<T> actualCalls = getActualCalls();
+
+        verifyCalls(expectedCalls, actualCalls);
     }
 
-    /** Mocks relevant calls to store metadata of log calls that were actually made. */
+    /**
+     * Mocks relevant calls in order to store metadata of log calls that were actually made.
+     * Subclasses are expected to call recordActualCall to store actual calls.
+     */
     protected abstract void mockLogCalls();
+
+    /**
+     * Return a set of {@link LogCall} to be verified.
+     *
+     * @param description test that was executed.
+     */
+    protected abstract Set<T> getExpectedLogCalls(Description description);
+
+    /**
+     * Subclasses are expected to use this method to record actual log call. Assumes only a single
+     * call is being recorded at once.
+     *
+     * @param actualCall Actual call to be recorded
+     */
+    protected void recordActualCall(T actualCall) {
+        mActualCalls.put(actualCall, mActualCalls.getOrDefault(actualCall, 0) + 1);
+    }
+
+    private void verifyCalls(Set<T> expectedCalls, Set<T> actualCalls) {
+        Log.d(mTag, "Total expected calls: " + expectedCalls.size());
+        Log.d(mTag, "Total actual calls: " + actualCalls.size());
+
+        // TODO (b/323000746) - Provide detailed error message (e.g. Expected X calls, but Y
+        //  actual calls etc.).
+        if (!Objects.equals(expectedCalls, actualCalls)) {
+            throw new IllegalStateException("Mismatch in log verification calls!");
+        }
+    }
+
+    private Set<T> getActualCalls() {
+        // At this point, the map of actual calls will no longer be updated. Therefore, it's safe
+        // to alter the times field in the actual LogCall objects.
+        mActualCalls
+                .keySet()
+                .forEach(actualLogCall -> actualLogCall.mTimes = mActualCalls.get(actualLogCall));
+
+        return mActualCalls.keySet();
+    }
 }
