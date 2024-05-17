e4528416bd0c99c5e77faa4a85401b5ffbb9621a
Alex Pankhurst <pankhurst@google.com>
2024-05-17 19:25:12 +0000

    Re-organize Cobalt testing code
    All test-only code is moved to a shared directory and packaged into its
    own library.
    
    Bug: 341338564
    Test: atest AdServicesCobaltUnitTests
    
    Change-Id: I2eba179d93fc3117e16013f69ff7499402217871

--- adservices/libraries/cobalt/java/com/android/cobalt/testing/crypto/HpkeEncryptFactory.java
+++ adservices/libraries/cobalt/java/com/android/cobalt/testing/crypto/HpkeEncryptFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.cobalt.crypto.testing;
+package com.android.cobalt.testing.crypto;
 
 import androidx.annotation.VisibleForTesting;
 
--- adservices/libraries/cobalt/java/com/android/cobalt/testing/crypto/NoOpEncrypter.java
+++ adservices/libraries/cobalt/java/com/android/cobalt/testing/crypto/NoOpEncrypter.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.cobalt.crypto.testing;
+package com.android.cobalt.testing.crypto;
 
 import android.annotation.NonNull;
 
