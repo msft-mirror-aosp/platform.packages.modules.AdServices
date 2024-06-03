f8ec4fd35413121e5d9bd934ecf880c39cdad378
Felipe Leme <felipeal@google.com>
2024-06-03 15:05:55 -0700

    Final (?!?!?!?!?) improvements on SyncCallback - floor 7.5
    Removed SyncCallbackTestCase.assumeCallbackSupportsSetCalled()
    
    Test: atest AdServicesSharedLibrariesUnitTests --iterations 10 --test-filter '.*Sync.*'
    Test: atest AdServicesSharedLibrariesUnitTests # Case, the name is Justin Case.
    
    Flag: TEST_ONLY
    Bug: 337014024
    
    Change-Id: Ia7582034ae70261ecc5f2b8100a6105c8a9f1666

--- shared/libraries/device-side/java/com/android/adservices/shared/testing/concurrency/DeviceSideSyncCallback.java
+++ shared/libraries/device-side/java/com/android/adservices/shared/testing/concurrency/DeviceSideSyncCallback.java
@@ -17,6 +17,8 @@ package com.android.adservices.shared.testing.concurrency;
 
 import android.os.IBinder;
 
+import com.google.common.annotations.VisibleForTesting;
+
 // TODO(b/337014024): it might be better to remove this class, or split it in 2 (for result and
 // resultless)
 /** Base class for device-side sync callbacks for testing. */
@@ -25,6 +27,11 @@ public abstract class DeviceSideSyncCallback extends AbstractSyncCallback
 
     protected DeviceSideSyncCallback(SyncCallbackSettings settings) {
         super(settings);
+    }
+
+    @VisibleForTesting
+    DeviceSideSyncCallback(AbstractSyncCallback realCallback, SyncCallbackSettings settings) {
+        super(realCallback, settings);
     }
 
     @Override
