d5cc7666a8b7fa6ca08f661b4ca1f6b7c2dcf1ea
Alice Liang <alliang@google.com>
2024-06-05 17:57:34 +0000

    Allow source to configure event level epsilon.
    Test: atest MeasurementDbMigratorV40Test
    Bug: 309964104
    Flag: EXEMPT (bug 309964104)
    
    Change-Id: If29e5d259b3c281853bc43da94890ec85d1da7aa

--- adservices/service-core/java/com/android/adservices/data/measurement/SqliteObjectMapper.java
+++ adservices/service-core/java/com/android/adservices/data/measurement/SqliteObjectMapper.java
@@ -250,6 +250,10 @@ public class SqliteObjectMapper {
                 cursor,
                 MeasurementTables.SourceContract.DESTINATION_LIMIT_PRIORITY,
                 builder::setDestinationLimitPriority);
+        setDoubleColumn(
+                cursor,
+                MeasurementTables.SourceContract.EVENT_LEVEL_EPSILON,
+                builder::setEventLevelEpsilon);
 
         return builder.build();
     }
