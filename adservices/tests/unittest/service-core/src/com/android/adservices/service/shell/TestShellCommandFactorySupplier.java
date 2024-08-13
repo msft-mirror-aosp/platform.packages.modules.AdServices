ab53c1fdcae4062306a63eed4320cb0301d56064
Cameron Brown <cambr@google.com>
2024-08-13 16:32:34 +0100

    feat(cli): Add on-device/server CA eligibility
    Introduces a new object called CustomAudienceEligibilityInfo currently
    only for the shell command package. Adds initial eligibility check based
    on if a CA is valid for each context. A follow-up change will add the
    different reasons a CA can be considered invalid.
    
    Fixes: 359560681
    Bug: 328432790
    Flag: EXEMPT (bug 337358613)
    Test: atest AdServicesServiceCoreUnitTests && \
      atest CtsAdServicesDebuggableDeviceTestCases
    Change-Id: I5e2ec659b0e0024f42c9c1e2fe9de76a347cf899

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/shell/TestShellCommandFactorySupplier.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/shell/TestShellCommandFactorySupplier.java
@@ -16,6 +16,8 @@
 
 package com.android.adservices.service.shell;
 
+import static android.adservices.customaudience.CustomAudienceFixture.CUSTOM_AUDIENCE_ACTIVE_FETCH_WINDOW_MS;
+
 import com.android.adservices.data.adselection.ConsentedDebugConfigurationDao;
 import com.android.adservices.data.customaudience.CustomAudienceDao;
 import com.android.adservices.data.signals.ProtectedSignalsDao;
@@ -28,6 +30,7 @@ import com.android.adservices.service.shell.signals.SignalsShellCommandFactory;
 
 import com.google.common.collect.ImmutableList;
 
+import java.time.Clock;
 import java.util.Objects;
 
 /**
@@ -81,7 +84,11 @@ public class TestShellCommandFactorySupplier extends ShellCommandFactorySupplier
     public ImmutableList<ShellCommandFactory> getAllShellCommandFactories() {
         return ImmutableList.of(
                 new CustomAudienceShellCommandFactory(
-                        mIsCustomAudienceCliEnabled, mBackgroundFetchRunner, mCustomAudienceDao),
+                        mIsCustomAudienceCliEnabled,
+                        mBackgroundFetchRunner,
+                        mCustomAudienceDao,
+                        Clock.systemUTC(),
+                        CUSTOM_AUDIENCE_ACTIVE_FETCH_WINDOW_MS),
                 new AdSelectionShellCommandFactory(
                         mIsConsentedDebugCliEnabled,
                         true,
