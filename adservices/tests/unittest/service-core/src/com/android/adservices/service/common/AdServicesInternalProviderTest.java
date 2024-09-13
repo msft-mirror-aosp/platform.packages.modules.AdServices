5b9de096664c8c2317e2b259611b4985b2a1f0b1
Felipe Leme <felipeal@google.com>
2024-09-12 17:21:10 -0700

    Improved AdServicesManager.getInstance(Context context)
    Ideally that method should not take a argument at all, but it's
    currently part of the AdServices framework (even though it's only
    used by the classes in the service APK) so its implementation cannot
    call ApplicationContextSingleton.
    
    Test: atest AdServicesServiceCoreUnitTests:AdServicesInternalProviderTest
    Bug: 311183933
    Bug: 366313883
    Flag: EXEMPT (bug 337358613)
    Change-Id: I3903ab04f0d0d66bda6ca3a2f59e8dc337af5412

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/common/AdServicesInternalProviderTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/common/AdServicesInternalProviderTest.java
@@ -22,6 +22,7 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import android.app.adservices.AdServicesManager;
 import android.content.Context;
 
 import com.android.adservices.common.AdServicesExtendedMockitoTestCase;
@@ -69,5 +70,16 @@ public final class AdServicesInternalProviderTest extends AdServicesExtendedMock
         String dump = dump(pw -> mProvider.dump(/* fd= */ null, pw, /* args= */ null));
 
         assertWithMessage("content of dump()").that(dump).contains(amcmDump);
+    }
+
+    @Test
+    @SpyStatic(AdServicesManager.class)
+    public void testDump_includesAdservicesManagerDump() throws Exception {
+        String mgrDump = "A Manager dumps no Name";
+        mockDump(() -> AdServicesManager.dump(any()), /* pwArgIndex= */ 0, mgrDump);
+
+        String dump = dump(pw -> mProvider.dump(/* fd= */ null, pw, /* args= */ null));
+
+        assertWithMessage("content of dump()").that(dump).contains(mgrDump);
     }
 }
