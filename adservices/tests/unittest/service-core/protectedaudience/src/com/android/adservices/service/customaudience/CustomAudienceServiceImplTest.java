0abd0c0d95cabc07c104212d0447a25743459505
leonhu <jingwenh@google.com>
2024-12-01 20:30:56 +0000

    Adds non-background CELs for  APIs
    * joinCustomAudience
    * leaveCustomAudience
    * fetchAndJoinCustomAudience
    * reportImpression
    * reportInteraction
    * scheduleCustomAudienceUpdate
    
    BYPASS_PS_FLAG_CHECK: adding CELs
    
    Flag: EXEMPT (bug 337358613 )
    Test: atest AdServicesServiceCoreProtectedAudienceUnitTests
    Bug: 329285478
    Change-Id: Ie9fdf384aef79e6c8148dea51de10fc7dc9d4726

--- adservices/tests/unittest/service-core/protectedaudience/src/com/android/adservices/service/customaudience/CustomAudienceServiceImplTest.java
+++ adservices/tests/unittest/service-core/protectedaudience/src/com/android/adservices/service/customaudience/CustomAudienceServiceImplTest.java
@@ -37,6 +37,11 @@ import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICE
 import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_API_CALLED__API_NAME__OVERRIDE_CUSTOM_AUDIENCE_REMOTE_INFO;
 import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_API_CALLED__API_NAME__REMOVE_CUSTOM_AUDIENCE_REMOTE_INFO_OVERRIDE;
 import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_API_CALLED__API_NAME__RESET_ALL_CUSTOM_AUDIENCE_OVERRIDES;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_GET_CALLING_UID_ILLEGAL_STATE;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__FETCH_AND_JOIN_CUSTOM_AUDIENCE;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__JOIN_CUSTOM_AUDIENCE;
+import static com.android.adservices.service.stats.AdServicesStatsLog.AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__LEAVE_CUSTOM_AUDIENCE;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.any;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.anyInt;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.anyString;
@@ -70,6 +75,7 @@ import android.os.Process;
 import android.os.RemoteException;
 
 import com.android.adservices.common.AdServicesExtendedMockitoTestCase;
+import com.android.adservices.common.logging.annotations.ExpectErrorLogUtilWithExceptionCall;
 import com.android.adservices.data.adselection.AppInstallDao;
 import com.android.adservices.data.adselection.FrequencyCapDao;
 import com.android.adservices.data.customaudience.CustomAudienceDao;
@@ -369,6 +375,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_GET_CALLING_UID_ILLEGAL_STATE,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__JOIN_CUSTOM_AUDIENCE,
+            throwable = IllegalStateException.class)
     public void testJoinCustomAudience_notInBinderThread() {
         mService =
                 new CustomAudienceServiceImpl(
@@ -446,6 +457,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            throwable = NullPointerException.class,
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__JOIN_CUSTOM_AUDIENCE)
     public void testJoinCustomAudience_nullInput() {
         assertThrows(
                 NullPointerException.class,
@@ -462,6 +478,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__JOIN_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testJoinCustomAudience_nullCallerPackageName() {
         assertThrows(
                 NullPointerException.class,
@@ -476,6 +497,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__JOIN_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testJoinCustomAudience_nullCallback() {
         assertThrows(
                 NullPointerException.class,
@@ -656,6 +682,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__FETCH_AND_JOIN_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testFetchCustomAudience_nullCallback() {
         assertThrows(
                 NullPointerException.class,
@@ -674,6 +705,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__FETCH_AND_JOIN_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testFetchCustomAudience_nullInput() {
         assertThrows(
                 NullPointerException.class,
@@ -687,6 +723,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_GET_CALLING_UID_ILLEGAL_STATE,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__FETCH_AND_JOIN_CUSTOM_AUDIENCE,
+            throwable = IllegalStateException.class)
     public void testFetchAndJoinCustomAudience_notInBinderThread() {
         mService =
                 new CustomAudienceServiceImpl(
@@ -896,6 +937,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_GET_CALLING_UID_ILLEGAL_STATE,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__LEAVE_CUSTOM_AUDIENCE,
+            throwable = IllegalStateException.class)
     public void testLeaveCustomAudience_notInBinderThread() {
         mService =
                 new CustomAudienceServiceImpl(
@@ -975,6 +1021,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__LEAVE_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testLeaveCustomAudience_nullOwner() {
         assertThrows(
                 NullPointerException.class,
@@ -992,6 +1043,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__LEAVE_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testLeaveCustomAudience_nullBuyer() {
         assertThrows(
                 NullPointerException.class,
@@ -1009,6 +1065,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__LEAVE_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testLeaveCustomAudience_nullName() {
         assertThrows(
                 NullPointerException.class,
@@ -1026,6 +1087,11 @@ public final class CustomAudienceServiceImplTest extends AdServicesExtendedMocki
     }
 
     @Test
+    @ExpectErrorLogUtilWithExceptionCall(
+            errorCode =
+                    AD_SERVICES_ERROR_REPORTED__ERROR_CODE__CUSTOM_AUDIENCE_SERVICE_NULL_ARGUMENT,
+            ppapiName = AD_SERVICES_ERROR_REPORTED__PPAPI_NAME__LEAVE_CUSTOM_AUDIENCE,
+            throwable = NullPointerException.class)
     public void testLeaveCustomAudience_nullCallback() {
         assertThrows(
                 NullPointerException.class,
