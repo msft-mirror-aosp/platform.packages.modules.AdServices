38a5c0c1d7fdf02c6f206cd4b52209470725f8c7
Felipe Leme <felipeal@google.com>
2025-01-13 13:01:24 -0800

    Moved DebugFlagsTest logic into DebugFlagsTestCase.
    That change will allow testing multiple implementations (like one
    used to DebugFlags on unit tests).
    
    Bug: 384798806
    Bug: 338067482
    Test: atest AdServicesServiceCoreUnitTests:DebugFlagsTest
    
    Flag: TEST_ONLY
    BYPASS_PS_FLAG_CHECK: test-only change
    
    Change-Id: Id477ad49a1d2d21705cd5fd5edf12aea15108139

--- adservices/tests/unittest/service-core/src/com/android/adservices/service/DebugFlagsTest.java
+++ adservices/tests/unittest/service-core/src/com/android/adservices/service/DebugFlagsTest.java
@@ -18,45 +18,8 @@ package com.android.adservices.service;
 
 import static com.android.adservices.service.CommonDebugFlags.DUMP_EQUALS;
 import static com.android.adservices.service.CommonDebugFlags.DUMP_PREFIX;
-import static com.android.adservices.service.DebugFlags.CONSENT_MANAGER_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlags.CONSENT_NOTIFICATION_ACTIVITY_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlags.CONSENT_NOTIFICATION_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlags.CONSENT_NOTIFIED_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlags.DEFAULT_AD_SELECTION_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_ATTRIBUTION_REPORTING_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_CONSENT_MANAGER_OTA_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlags.DEFAULT_DEVELOPER_SESSION_FEATURE_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_FLEDGE_AUCTION_SERVER_CONSENTED_DEBUGGING_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_FLEDGE_CONSENTED_DEBUGGING_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_FLEDGE_CUSTOM_AUDIENCE_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_FORCED_ENCODING_COMPLETE_BROADCAST_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_JS_ISOLATE_CONSOLE_MESSAGES_IN_LOGS_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_PROTECTED_APP_SIGNALS_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_PROTECTED_APP_SIGNALS_ENCODER_LOGIC_REGISTERED_BROADCAST_ENABLED;
-import static com.android.adservices.service.DebugFlags.DEFAULT_RECORD_TOPICS_COMPLETE_BROADCAST_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_AD_SELECTION_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_AD_SERVICES_JS_ISOLATE_CONSOLE_MESSAGES_IN_LOGS_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_ATTRIBUTION_REPORTING_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_CONSENT_MANAGER_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_CONSENT_MANAGER_OTA_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_CONSENT_NOTIFICATION_ACTIVITY_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_CONSENT_NOTIFICATION_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_CONSENT_NOTIFIED_DEBUG_MODE;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_DEVELOPER_SESSION_FEATURE_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_FLEDGE_AUCTION_SERVER_CONSENTED_DEBUGGING_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_FLEDGE_IS_CONSENTED_DEBUGGING_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_FLEDGE_IS_CUSTOM_AUDIENCE_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_FORCED_ENCODING_JOB_COMPLETE_BROADCAST_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_PROTECTED_APP_SIGNALS_CLI_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_PROTECTED_APP_SIGNALS_ENCODER_LOGIC_REGISTERED_BROADCAST_ENABLED;
-import static com.android.adservices.service.DebugFlagsConstants.KEY_RECORD_TOPICS_COMPLETE_BROADCAST_ENABLED;
 import static com.android.adservices.shared.meta_testing.FlagsTestLittleHelper.expectDumpHasAllFlags;
 
-import static com.google.common.truth.Truth.assertWithMessage;
-
-import android.util.Log;
-
-import com.android.adservices.common.AdServicesExtendedMockitoTestCase;
 import com.android.adservices.mockito.AdServicesExtendedMockitoRule;
 import com.android.adservices.service.fixture.TestableSystemProperties;
 import com.android.modules.utils.build.SdkLevel;
@@ -64,11 +27,13 @@ import com.android.modules.utils.testing.ExtendedMockitoRule;
 
 import org.junit.Test;
 
-/** Unit tests for {@link com.android.adservices.service.DebugFlags} */
 @ExtendedMockitoRule.SpyStatic(SdkLevel.class)
-public final class DebugFlagsTest extends AdServicesExtendedMockitoTestCase {
+public final class DebugFlagsTest extends DebugFlagsTestCase<DebugFlags> {
 
-    private final DebugFlags mDebugFlags = DebugFlags.getInstance();
+    @Override
+    protected DebugFlags newInstance() {
+        return DebugFlags.getInstance();
+    }
 
     @Override
     protected AdServicesExtendedMockitoRule getAdServicesExtendedMockitoRule() {
@@ -78,164 +43,26 @@ public final class DebugFlagsTest extends AdServicesExtendedMockitoTestCase {
     }
 
     @Test
-    public void testConsentNotificationDebugMode() {
-        testDebugFlag(
-                KEY_CONSENT_NOTIFICATION_DEBUG_MODE,
-                CONSENT_NOTIFICATION_DEBUG_MODE,
-                DebugFlags::getConsentNotificationDebugMode);
-    }
-
-    @Test
-    public void testConsentNotifiedDebugMode() {
-        testDebugFlag(
-                KEY_CONSENT_NOTIFIED_DEBUG_MODE,
-                CONSENT_NOTIFIED_DEBUG_MODE,
-                DebugFlags::getConsentNotifiedDebugMode);
-    }
-
-    @Test
-    public void testConsentNotificationActivityDebugMode() {
-        testDebugFlag(
-                KEY_CONSENT_NOTIFICATION_ACTIVITY_DEBUG_MODE,
-                CONSENT_NOTIFICATION_ACTIVITY_DEBUG_MODE,
-                DebugFlags::getConsentNotificationActivityDebugMode);
-    }
-
-    @Test
-    public void testConsentManagerDebugMode() {
-        testDebugFlag(
-                KEY_CONSENT_MANAGER_DEBUG_MODE,
-                CONSENT_MANAGER_DEBUG_MODE,
-                DebugFlags::getConsentManagerDebugMode);
-    }
-
-    @Test
-    public void testConsentManagerOTADebugMode() {
-        testDebugFlag(
-                KEY_CONSENT_MANAGER_OTA_DEBUG_MODE,
-                DEFAULT_CONSENT_MANAGER_OTA_DEBUG_MODE,
-                DebugFlags::getConsentManagerOTADebugMode);
-    }
-
-    @Test
-    public void testDeveloperSessionFeatureEnabled() {
-        testDebugFlag(
-                KEY_DEVELOPER_SESSION_FEATURE_ENABLED,
-                DEFAULT_DEVELOPER_SESSION_FEATURE_ENABLED,
-                DebugFlags::getDeveloperSessionFeatureEnabled);
-    }
-
-    @Test
-    public void testProtectedAppSignalsCommandsEnabled() {
-        testDebugFlag(
-                KEY_PROTECTED_APP_SIGNALS_CLI_ENABLED,
-                DEFAULT_PROTECTED_APP_SIGNALS_CLI_ENABLED,
-                DebugFlags::getProtectedAppSignalsCommandsEnabled);
-    }
-
-    @Test
-    public void testAdSelectionCommandsEnabled() {
-        testDebugFlag(
-                KEY_AD_SELECTION_CLI_ENABLED,
-                DEFAULT_AD_SELECTION_CLI_ENABLED,
-                DebugFlags::getAdSelectionCommandsEnabled);
-    }
-
-    @Test
-    public void testGetFledgeAuctionServerConsentedDebuggingEnabled() {
-        testDebugFlag(
-                KEY_FLEDGE_AUCTION_SERVER_CONSENTED_DEBUGGING_ENABLED,
-                DEFAULT_FLEDGE_AUCTION_SERVER_CONSENTED_DEBUGGING_ENABLED,
-                DebugFlags::getFledgeAuctionServerConsentedDebuggingEnabled);
-    }
-
-    @Test
-    public void testGetFledgConsentedDebuggingCliEnabledStatusFlag() {
-        testDebugFlag(
-                KEY_FLEDGE_IS_CONSENTED_DEBUGGING_CLI_ENABLED,
-                DEFAULT_FLEDGE_CONSENTED_DEBUGGING_CLI_ENABLED,
-                DebugFlags::getFledgeConsentedDebuggingCliEnabledStatus);
-    }
-
-    @Test
-    public void testGetFledgeCustomAudienceCliEnabledStatusFlag() {
-        testDebugFlag(
-                KEY_FLEDGE_IS_CUSTOM_AUDIENCE_CLI_ENABLED,
-                DEFAULT_FLEDGE_CUSTOM_AUDIENCE_CLI_ENABLED,
-                DebugFlags::getFledgeCustomAudienceCLIEnabledStatus);
-    }
-
-    @Test
-    public void testGetAdServicesJsIsolateConsoleMessagesInLogsEnabled() {
-        testDebugFlag(
-                KEY_AD_SERVICES_JS_ISOLATE_CONSOLE_MESSAGES_IN_LOGS_ENABLED,
-                DEFAULT_JS_ISOLATE_CONSOLE_MESSAGES_IN_LOGS_ENABLED,
-                DebugFlags::getAdServicesJsIsolateConsoleMessagesInLogsEnabled);
-    }
-
-    @Test
-    public void testRecordTopicsCompleteBroadcastEnabled() {
-        testDebugFlag(
-                KEY_RECORD_TOPICS_COMPLETE_BROADCAST_ENABLED,
-                DEFAULT_RECORD_TOPICS_COMPLETE_BROADCAST_ENABLED,
-                DebugFlags::getRecordTopicsCompleteBroadcastEnabled);
-    }
-
-    @Test
-    public void testGetProtectedAppSignalsEncoderLogicRegisteredBroadcastEnabled() {
-        testDebugFlag(
-                KEY_PROTECTED_APP_SIGNALS_ENCODER_LOGIC_REGISTERED_BROADCAST_ENABLED,
-                DEFAULT_PROTECTED_APP_SIGNALS_ENCODER_LOGIC_REGISTERED_BROADCAST_ENABLED,
-                DebugFlags::getProtectedAppSignalsEncoderLogicRegisteredBroadcastEnabled);
-    }
-
-    @Test
-    public void testGetForcedEncodingJobCompleteBroadcastEnabled() {
-        testDebugFlag(
-                KEY_FORCED_ENCODING_JOB_COMPLETE_BROADCAST_ENABLED,
-                DEFAULT_FORCED_ENCODING_COMPLETE_BROADCAST_ENABLED,
-                DebugFlags::getForcedEncodingJobCompleteBroadcastEnabled);
-    }
-
-    @Test
-    public void testAttributionReportingCliEnabled() {
-        testDebugFlag(
-                KEY_ATTRIBUTION_REPORTING_CLI_ENABLED,
-                DEFAULT_ATTRIBUTION_REPORTING_CLI_ENABLED,
-                DebugFlags::getAttributionReportingCommandsEnabled);
-    }
-
-    @Test
     public void testDump() throws Exception {
+        var debugFlags = newInstance();
+
         expectDumpHasAllFlags(
                 expect,
                 DebugFlagsConstants.class,
-                pw -> mDebugFlags.dump(pw),
+                pw -> debugFlags.dump(pw),
                 flag -> (DUMP_PREFIX + flag.second + DUMP_EQUALS));
         // Must also check the flags from its superclass
         expectDumpHasAllFlags(
                 expect,
                 CommonDebugFlagsConstants.class,
-                pw -> mDebugFlags.dump(pw),
+                pw -> debugFlags.dump(pw),
                 flag -> (DUMP_PREFIX + flag.second + DUMP_EQUALS));
     }
 
-    private void testDebugFlag(
-            String flagName, Boolean defaultValue, Flaginator<DebugFlags, Boolean> flaginator) {
-        // Without any overriding, the value is the hard coded constant.
-        assertWithMessage("before overriding")
-                .that(flaginator.getFlagValue(mDebugFlags))
-                .isEqualTo(defaultValue);
-
-        boolean phOverridingValue = !defaultValue;
-        setSystemProperty(flagName, String.valueOf(phOverridingValue));
-        assertWithMessage("after overriding")
-                .that(flaginator.getFlagValue(mDebugFlags))
-                .isEqualTo(phOverridingValue);
-    }
-
-    private void setSystemProperty(String name, String value) {
-        Log.v(mTag, "setSystemProperty(): " + name + "=" + value);
-        TestableSystemProperties.set(PhFlags.getSystemPropertyName(name), "" + value);
+    @Override
+    protected void setDebugFlag(String name, String value) {
+        String realName = PhFlags.getSystemPropertyName(name);
+        mLog.v("setDebugFlag(%s, %s): setting SystemProperty %s", name, value, realName);
+        TestableSystemProperties.set(realName, "" + value);
     }
 }
