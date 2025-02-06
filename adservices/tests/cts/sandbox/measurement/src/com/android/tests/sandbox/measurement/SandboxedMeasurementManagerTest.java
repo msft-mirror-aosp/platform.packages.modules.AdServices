745648870b918ec10b6c92d30ada959b0b6bf405
Dang Le <danghle@google.com>
2025-02-05 16:12:21 -0800

    Fix Measurement CTS tests
    - Add shell command to kill adservices.api process before each test to prevent unexpected flag behavior between tests.
    
    Bug: 366362576
    Bug: 363524482
    Test: atest MeasurementManagerCtsTest; atest SandboxedMeasurementManagerTest
    Flag: EXEMPT bug fix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c54ef3f90f1333735b17d5330977781ed4f7d47c)
    Merged-In: I32ee95071d4f8c2c1123e3f50a6a2dfa70a5f3be
    Change-Id: I32ee95071d4f8c2c1123e3f50a6a2dfa70a5f3be

--- adservices/tests/cts/sandbox/measurement/src/com/android/tests/sandbox/measurement/SandboxedMeasurementManagerTest.java
+++ adservices/tests/cts/sandbox/measurement/src/com/android/tests/sandbox/measurement/SandboxedMeasurementManagerTest.java
@@ -16,25 +16,29 @@
 
 package com.android.tests.sandbox.measurement;
 
+import static com.android.adservices.service.FlagsConstants.KEY_ADID_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_DISABLE_MEASUREMENT_ENROLLMENT_CHECK;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_API_DELETE_REGISTRATIONS_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_API_REGISTER_SOURCE_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_API_REGISTER_TRIGGER_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_API_REGISTER_WEB_SOURCE_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_API_REGISTER_WEB_TRIGGER_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_API_STATUS_KILL_SWITCH;
+import static com.android.adservices.service.FlagsConstants.KEY_MEASUREMENT_KILL_SWITCH;
 
 import android.app.sdksandbox.SdkSandboxManager;
 import android.app.sdksandbox.testutils.FakeLoadSdkCallback;
 import android.app.sdksandbox.testutils.SdkSandboxDeviceSupportedRule;
-import android.content.Context;
 import android.os.Bundle;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-
 import com.android.adservices.common.AdservicesTestHelper;
-import com.android.compatibility.common.util.ShellUtils;
+import com.android.adservices.shared.testing.annotations.SetFlagDisabled;
+import com.android.adservices.shared.testing.annotations.SetFlagEnabled;
 
 import org.junit.After;
-import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import java.time.Duration;
 import java.util.concurrent.Executor;
@@ -44,41 +48,33 @@ import java.util.concurrent.TimeoutException;
 /*
  * Test Measurement APIs running within the Sandbox.
  */
-@RunWith(JUnit4.class)
-public class SandboxedMeasurementManagerTest {
+@SetFlagDisabled(KEY_ADID_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_API_DELETE_REGISTRATIONS_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_API_REGISTER_SOURCE_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_API_REGISTER_TRIGGER_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_API_REGISTER_WEB_SOURCE_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_API_REGISTER_WEB_TRIGGER_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_API_STATUS_KILL_SWITCH)
+@SetFlagDisabled(KEY_MEASUREMENT_KILL_SWITCH)
+@SetFlagEnabled(KEY_DISABLE_MEASUREMENT_ENROLLMENT_CHECK)
+public final class SandboxedMeasurementManagerTest extends CtsSandboxedMeasurementManagerTestCase {
     private static final Executor CALLBACK_EXECUTOR = Executors.newCachedThreadPool();
     private static final String SDK_NAME = "com.android.tests.providers.sdkmeasurement";
-
-    private static final Context sContext =
-            InstrumentationRegistry.getInstrumentation().getContext();
 
     @Rule(order = 0)
     public final SdkSandboxDeviceSupportedRule supportedRule = new SdkSandboxDeviceSupportedRule();
 
     @Before
     public void setup() throws TimeoutException {
-        // Skip the test if it runs on unsupported platforms.
-        Assume.assumeTrue(AdservicesTestHelper.isDeviceSupported());
-
+        // Kill adservices process to avoid interfering from other tests.
+        AdservicesTestHelper.killAdservicesProcess(mContext);
         // Start a foreground activity
-        SimpleActivity.startAndWaitForSimpleActivity(sContext, Duration.ofMillis(10_000));
-
-        enforceMeasurementEnrollmentCheck(true);
-
-        // Allow sandbox package name to be able to execute Measurement APIs
-        allowSandboxPackageNameAccessMeasurementApis();
-
-        overrideMeasurementKillSwitches(true);
+        SimpleActivity.startAndWaitForSimpleActivity(mContext, Duration.ofMillis(10_000));
     }
 
     @After
     public void shutDown() {
-        SimpleActivity.stopSimpleActivity(sContext);
-
-        // Reset back the original values.
-        resetAllowSandboxPackageNameAccessMeasurementApis();
-
-        overrideMeasurementKillSwitches(false);
+        SimpleActivity.stopSimpleActivity(mContext);
     }
 
     @Test
@@ -93,66 +89,16 @@ public class SandboxedMeasurementManagerTest {
         // In this test, we use the loadSdk's callback as a 2-way communications between the Test
         // app (this class) and the Sdk running within the Sandbox process.
 
-        final SdkSandboxManager sdkSandboxManager =
-                sContext.getSystemService(SdkSandboxManager.class);
+        SdkSandboxManager sdkSandboxManager = mContext.getSystemService(SdkSandboxManager.class);
 
         // The enrolled URLs should time out when registering to them, because we don't control
         // them; each timeout is 5 seconds, plus some wiggle room
-        final FakeLoadSdkCallback callback = new FakeLoadSdkCallback(25);
+        FakeLoadSdkCallback callback = new FakeLoadSdkCallback(25);
 
         // Load SdkMeasurement
         sdkSandboxManager.loadSdk(SDK_NAME, new Bundle(), CALLBACK_EXECUTOR, callback);
 
         // This verifies SdkMeasurement finished without errors.
         callback.assertLoadSdkIsSuccessful();
-    }
-
-    private void enforceMeasurementEnrollmentCheck(boolean shouldEnforce) {
-        ShellUtils.runShellCommand(
-                "device_config put adservices disable_measurement_enrollment_check %s",
-                !shouldEnforce);
-    }
-
-    private void allowSandboxPackageNameAccessMeasurementApis() {
-        final String sdkSbxName = "com.android.tests.sandbox.measurement";
-        ShellUtils.runShellCommand(
-                "device_config put adservices web_context_client_allow_list " + sdkSbxName);
-    }
-
-    private void resetAllowSandboxPackageNameAccessMeasurementApis() {
-        ShellUtils.runShellCommand(
-                "device_config put adservices web_context_client_allow_list null");
-    }
-
-    // Override measurement related kill switch to ignore the effect of actual PH values.
-    // If isOverride = true, override measurement related kill switch to OFF to allow adservices
-    // If isOverride = false, override measurement related kill switch to meaningless value so that
-    // PhFlags will use the default value.
-    private void overrideMeasurementKillSwitches(boolean isOverride) {
-        String overrideString = isOverride ? "false" : "null";
-        ShellUtils.runShellCommand(
-                "device_config put adservices global_kill_switch " + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_kill_switch " + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_api_register_source_kill_switch "
-                        + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_api_register_trigger_kill_switch "
-                        + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_api_register_web_source_kill_switch "
-                        + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_api_register_web_trigger_kill_switch "
-                        + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_api_delete_registrations_kill_switch "
-                        + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices measurement_api_status_kill_switch "
-                        + overrideString);
-        ShellUtils.runShellCommand(
-                "device_config put adservices adid_kill_switch " + overrideString);
     }
 }
