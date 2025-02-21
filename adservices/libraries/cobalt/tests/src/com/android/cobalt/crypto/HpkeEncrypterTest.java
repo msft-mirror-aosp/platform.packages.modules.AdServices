352c0802aeb069287b769479c2cb7f3c0c08602d
Hao Liu <haoliuu@google.com>
2025-02-21 17:59:47 +0000

    Change CobaltFactory to use CobaltPublicKeyLoader to get public keys
    This change removes PublicKeys file and CobaltFactory will fetch keys
    from asset file.
    
    Test: atest AdServicesServiceCoreUnitTests AdServicesCobaltUnitTests
    Bug: 364697156
    PS_FLAG: cobalt_logging_enabled
    Flag: EXEMPT (bug 337358613
    
    Change-Id: Id9833e376b44af189f14566ab23448244ddf7fa0

--- adservices/libraries/cobalt/tests/src/com/android/cobalt/crypto/HpkeEncrypterTest.java
+++ adservices/libraries/cobalt/tests/src/com/android/cobalt/crypto/HpkeEncrypterTest.java
@@ -16,21 +16,13 @@
 
 package com.android.cobalt.crypto;
 
-import static com.android.cobalt.crypto.PublicKeys.ANALYZER_KEY_DEV;
-import static com.android.cobalt.crypto.PublicKeys.ANALYZER_KEY_INDEX_DEV;
-import static com.android.cobalt.crypto.PublicKeys.ANALYZER_KEY_INDEX_PROD;
-import static com.android.cobalt.crypto.PublicKeys.SHUFFLER_KEY_DEV;
-import static com.android.cobalt.crypto.PublicKeys.SHUFFLER_KEY_INDEX_DEV;
-import static com.android.cobalt.crypto.PublicKeys.SHUFFLER_KEY_INDEX_PROD;
-
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-
-import androidx.test.runner.AndroidJUnit4;
 
 import com.android.adservices.cobalt.HpkeEncryptImpl;
 import com.android.cobalt.CobaltPipelineType;
@@ -46,26 +38,73 @@ import com.google.cobalt.ObservationToEncrypt;
 import com.google.protobuf.ByteString;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
-@RunWith(AndroidJUnit4.class)
 public class HpkeEncrypterTest {
+    @Rule public final MockitoRule mockito = MockitoJUnit.rule();
     // KeyIndex values for testing.
     private static final int SHUFFLER_KEY_INDEX_VALUE = 1;
     private static final int ANALYZER_KEY_INDEX_VALUE = 1337;
-    private HpkeEncrypter mHpkeEncrypter;
+    static final byte[] SHUFFLER_KEY_PROD =
+            new byte[] {
+                -111, -111, 86, 123, -6, -114, -109, 121, -84, -113, -92, -5, 50, 103, 22, -53, 103,
+                -57, 97, 11, 80, -5, 105, -26, -122, 106, 65, 107, -32, -74, -23, 10
+            };
+    private static final byte[] SHUFFLER_KEY_DEV =
+            new byte[] {
+                -90, -73, 32, -62, 119, -72, 48, -40, -127, -103, -7, -58, 35, -88, -4, 45, 33, 21,
+                32, 48, 42, 43, 89, 33, -43, -81, -64, 111, 118, 76, 77, 32
+            };
+    static final byte[] ANALYZER_KEY_PROD =
+            new byte[] {
+                75, -121, 55, -37, -24, -80, -119, -113, -64, 25, -91, 114, -56, -23, 108, 5, 90,
+                -3, 24, -62, 1, 109, 51, 123, -88, 36, 36, 0, 51, 104, -37, 1
+            };
+    private static final byte[] ANALYZER_KEY_DEV =
+            new byte[] {
+                -5, -81, 123, 9, -16, -83, -75, -106, 122, -13, 111, -106, 123, -65, -7, -78, 125,
+                107, -23, 69, 120, -59, 40, 19, 6, 92, -119, 6, -58, 126, 125, 41
+            };
+    private static final byte[] SHUFFLER_CONTEXT_INFO_BYTES =
+            "cobalt-1.0-shuffler".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] ANALYZER_CONTEXT_INFO_BYTES =
+            "cobalt-1.0-analyzer".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] EMPTY_KEY = new byte[] {};
+    private static final int SHUFFLER_KEY_INDEX_PROD = 11;
+    private static final int SHUFFLER_KEY_INDEX_DEV = 9;
+    private static final int ANALYZER_KEY_INDEX_PROD = 12;
+    private static final int ANALYZER_KEY_INDEX_DEV = 10;
+    private static final int X25519_PUBLIC_VALUE_LEN = 32;
 
     private static final int TEST_METRIC_ID = 25;
     private static final ByteString TEST_CONTRIBUTION_ID = ByteString.copyFromUtf8("testContId");
 
+    private HpkeEncrypter mHpkeEncrypter;
+    @Mock private PublicEncryptionKeys mPublicEncryptionKeys;
+
     @Before
     public void setUp() {
+        when(mPublicEncryptionKeys.getAnalyzerKeyIndexDev()).thenReturn(ANALYZER_KEY_INDEX_DEV);
+        when(mPublicEncryptionKeys.getAnalyzerKeyIndexProd()).thenReturn(ANALYZER_KEY_INDEX_PROD);
+        when(mPublicEncryptionKeys.getShufflerKeyIndexDev()).thenReturn(SHUFFLER_KEY_INDEX_DEV);
+        when(mPublicEncryptionKeys.getShufflerKeyIndexProd()).thenReturn(SHUFFLER_KEY_INDEX_PROD);
+        when(mPublicEncryptionKeys.getX25519PublicValueLen()).thenReturn(X25519_PUBLIC_VALUE_LEN);
+        when(mPublicEncryptionKeys.getShufflerContextInfoBytes())
+                .thenReturn(SHUFFLER_CONTEXT_INFO_BYTES);
+        when(mPublicEncryptionKeys.getAnalyzerContextInfoBytes())
+                .thenReturn(ANALYZER_CONTEXT_INFO_BYTES);
+
         mHpkeEncrypter =
                 new HpkeEncrypter(
                         HpkeEncryptFactory.noOpHpkeEncrypt(),
+                        mPublicEncryptionKeys,
                         SHUFFLER_KEY_DEV,
                         SHUFFLER_KEY_INDEX_VALUE,
                         ANALYZER_KEY_DEV,
@@ -156,6 +195,7 @@ public class HpkeEncrypterTest {
         mHpkeEncrypter =
                 new HpkeEncrypter(
                         new HpkeEncryptImpl(),
+                        mPublicEncryptionKeys,
                         SHUFFLER_KEY_DEV,
                         SHUFFLER_KEY_INDEX_VALUE,
                         ANALYZER_KEY_DEV,
@@ -180,6 +220,7 @@ public class HpkeEncrypterTest {
         mHpkeEncrypter =
                 new HpkeEncrypter(
                         new HpkeEncryptImpl(),
+                        mPublicEncryptionKeys,
                         SHUFFLER_KEY_DEV,
                         SHUFFLER_KEY_INDEX_VALUE,
                         ANALYZER_KEY_DEV,
@@ -216,6 +257,7 @@ public class HpkeEncrypterTest {
         mHpkeEncrypter =
                 new HpkeEncrypter(
                         HpkeEncryptFactory.noOpHpkeEncrypt(),
+                        mPublicEncryptionKeys,
                         non32BytePublicKey,
                         SHUFFLER_KEY_INDEX_VALUE,
                         non32BytePublicKey,
@@ -238,6 +280,7 @@ public class HpkeEncrypterTest {
         mHpkeEncrypter =
                 new HpkeEncrypter(
                         HpkeEncryptFactory.emptyHpkeEncrypt(),
+                        mPublicEncryptionKeys,
                         SHUFFLER_KEY_DEV,
                         SHUFFLER_KEY_INDEX_VALUE,
                         ANALYZER_KEY_DEV,
@@ -248,18 +291,28 @@ public class HpkeEncrypterTest {
 
     @Test
     public void checkKeyIndices_prodEnvironment() {
+        when(mPublicEncryptionKeys.getShufflerKeyProd()).thenReturn(SHUFFLER_KEY_PROD);
+        when(mPublicEncryptionKeys.getAnalyzerKeyProd()).thenReturn(ANALYZER_KEY_PROD);
+
         HpkeEncrypter hpkeEncrypter =
                 HpkeEncrypter.createForEnvironment(
-                        HpkeEncryptFactory.noOpHpkeEncrypt(), CobaltPipelineType.PROD);
+                        HpkeEncryptFactory.noOpHpkeEncrypt(),
+                        CobaltPipelineType.PROD,
+                        mPublicEncryptionKeys);
         assertThat(hpkeEncrypter.mShufflerKeyIndex).isEqualTo(SHUFFLER_KEY_INDEX_PROD);
         assertThat(hpkeEncrypter.mAnalyzerKeyIndex).isEqualTo(ANALYZER_KEY_INDEX_PROD);
     }
 
     @Test
     public void checkKeyIndices_devEnvironment() {
+        when(mPublicEncryptionKeys.getShufflerKeyDev()).thenReturn(SHUFFLER_KEY_DEV);
+        when(mPublicEncryptionKeys.getAnalyzerKeyDev()).thenReturn(ANALYZER_KEY_DEV);
+
         HpkeEncrypter hpkeEncrypter =
                 HpkeEncrypter.createForEnvironment(
-                        HpkeEncryptFactory.noOpHpkeEncrypt(), CobaltPipelineType.DEV);
+                        HpkeEncryptFactory.noOpHpkeEncrypt(),
+                        CobaltPipelineType.DEV,
+                        mPublicEncryptionKeys);
         assertThat(hpkeEncrypter.mShufflerKeyIndex).isEqualTo(SHUFFLER_KEY_INDEX_DEV);
         assertThat(hpkeEncrypter.mAnalyzerKeyIndex).isEqualTo(ANALYZER_KEY_INDEX_DEV);
     }
