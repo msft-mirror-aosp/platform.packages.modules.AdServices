94c2adfb3af1702a943562d9930e51c61fd39c17
Sanjana Sunil <sanjanasunil@google.com>
2025-02-27 14:36:55 +0000

    Avoid deadlock when calling stopSdkSandboxService
    A deadlock can happen between SdkSandboxManagerService (SSMS) and
    ActivityManagerService (AMS) in the following situation:
    1) The sandbox tries to register a broadcast receiver using AMS. AMS
       locks on an AMS lock and then calls into SSMS to check if the sandbox
       can register the receiver. It attempts to hold the SSMS lock.
    2) In the meantime, if the sandbox is to be killed, SSMS holding an SSMS
       lock calls into AMS and then attempts to hold the AMS lock causing a
       deadlock.
    
    This change is a quick fix for this - no lock is held when SSMS calls
    stopSdkSandboxService on AMS.
    
    Note that there could be several other existing instances apart from
    stopSdkSandboxService where a deadlock is caused because SSMS calls into
    AMS (or another service) and the other service calls into SSMS. These
    will be investigated and fixed in future changes (perhaps by using more
    locks instead of a shared one).
    
    Bug: 398296192
    Test: atest
    Flag: EXEMPT b/337358613
    PS_FLAG: fix_deadlock_bug_398296192
    
    Change-Id: I44d50db8fa4a57d556759bbdf49bbc82ac257d85

--- sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
+++ sdksandbox/service/java/com/android/server/sdksandbox/SdkSandboxManagerService.java
@@ -1080,6 +1080,8 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
     }
 
     private void onAppDeath(CallingInfo callingInfo) {
+        boolean isStopSandboxDeadlockFixed =
+                mSdkSandboxSettingsListener.getStopSandboxDeadlockFix();
         synchronized (mLock) {
             Log.d(TAG, "App " + callingInfo + " has died, cleaning up associated sandbox info");
             mSandboxLifecycleCallbacks.remove(callingInfo);
@@ -1091,6 +1093,12 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
             mSyncDataCallbacks.remove(callingInfo);
             mLoadSdkSessions.remove(callingInfo);
             mHeldInterfaces.remove(callingInfo);
+            if (!isStopSandboxDeadlockFixed) {
+                stopSdkSandboxService(callingInfo, "Caller " + callingInfo + " has died");
+                mServiceProvider.onAppDeath(callingInfo);
+            }
+        }
+        if (isStopSandboxDeadlockFixed) {
             stopSdkSandboxService(callingInfo, "Caller " + callingInfo + " has died");
             mServiceProvider.onAppDeath(callingInfo);
         }
@@ -1668,8 +1676,20 @@ public class SdkSandboxManagerService extends ISdkSandboxManager.Stub {
     }
 
     void stopAllSandboxes() {
-        synchronized (mLock) {
-            stopAllSandboxesLocked();
+        boolean isStopSandboxDeadlockFixed =
+                mSdkSandboxSettingsListener.getStopSandboxDeadlockFix();
+        if (isStopSandboxDeadlockFixed) {
+            synchronized (mLock) {
+                stopAllSandboxesLocked();
+            }
+        } else {
+            Set<CallingInfo> callingInfos;
+            synchronized (mLock) {
+                callingInfos = mLoadSdkSessions.keySet();
+            }
+            for (CallingInfo callingInfo : callingInfos) {
+                stopSdkSandboxService(callingInfo, "SDK sandbox killswitch enabled");
+            }
         }
     }
 
