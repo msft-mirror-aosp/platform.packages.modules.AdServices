16d6c3b7c367507268aff91575be592d9c9d6b0a
Alexey Patosin <apatosin@google.com>
2025-02-28 16:10:26 +0000

    Update RbATraces to use async methods
    Bug: 389598844
    PS_FLAG: enable_rb_atrace
    Flag: EXEMPT (bug 337358613  )
    FLAG_GUARD: enable_rb_atrace
    Test: Presubmit, atest AdServicesServiceCoreUnitTests
    
    Change-Id: I925f7cef973f050e0d66d6c5cefb493496ea7e4a

---
+++ adservices/service-core/java/com/android/adservices/service/profiling/TracingNames.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.adservices.service.profiling;
+
+/** Metric, class and method names used to name tracing points. */
+public final class TracingNames {
+    public static final String CLASS_NAME_AD_ID_SERVICE = "AdIdService";
+    public static final String CLASS_NAME_ATTRIBUTION_JOB_SERVICE = "AttributionJobService";
+    public static final String CLASS_NAME_ATTRIBUTION_FALLBACK_JOB_SERVICE =
+            "AttributionFallbackJobService";
+    public static final String CLASS_NAME_TOPICS_SERVICE = "TopicsService";
+
+    public static final String METHOD_NAME_GET_AD_ID = "getAdId";
+    public static final String METHOD_NAME_ON_START_JOB = "onStartJob";
+    public static final String METHOD_NAME_GET_TOPICS = "getTopics";
+}
