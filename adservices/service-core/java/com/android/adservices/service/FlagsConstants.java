f3a00b729d73c0ab580e206a8bf91f02ac5c9259
Alexey Patosin <apatosin@google.com>
2025-03-18 10:50:18 +0000

    Fix the feature flag name to follow the format FeatureName__flagName.
    Bug: 404466729
    Test: Presubmit
    Flag: EXEMPT Adding new flag
    BYPASS_PS_FLAG_CHECK: Updating the wrong flag name
    SERVER_AUTO_GENERATED_FLAG_NAME: CrystalballMetrics__enable_rb_atrace
    
    Change-Id: I59e5da281bc43ff38cde81317f8b05f24838e637

--- adservices/service-core/java/com/android/adservices/service/FlagsConstants.java
+++ adservices/service-core/java/com/android/adservices/service/FlagsConstants.java
@@ -1766,7 +1766,7 @@ public final class FlagsConstants {
             "Fledge__enable_prod_debug_in_auction_server";
 
     /** Key to enable the AdServices latency metrics {@code RbATrace}. */
-    public static final String KEY_ENABLE_RB_ATRACE = "enable_rb_atrace";
+    public static final String KEY_ENABLE_RB_ATRACE = "CrystalballMetrics__enable_rb_atrace";
 
     /** Key to enable the package deny list for source registration */
     public static final String KEY_MSMT_REGISTER_SOURCE_PACKAGE_DENY_LIST =
@@ -1800,4 +1800,3 @@ public final class FlagsConstants {
     public static final String KEY_MEASUREMENT_DEBUG_JOIN_KEYS_SAMPLE_RATE =
             "Measurement__debug_join_keys_sample_rate";
 }
-
