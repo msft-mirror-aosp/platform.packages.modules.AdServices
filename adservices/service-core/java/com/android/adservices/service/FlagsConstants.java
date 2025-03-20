d50fd087f3fd0160ded361d2860a1dcfc6e49b4b
Jonathan Pierce <jonathanpie@google.com>
2025-03-20 22:57:03 +0000

    Add flags for setting the Aggregation Coordinator Origin for internal Count Unique testing.
    Bug: 401306435
    Test: atest AdServicesServiceCoreUnitTests:PhFlagsTest
    Test: atest AdServicesServiceCoreUnitTests:FlagsTest
    Flag: EXEMPT (bug 337358613)
    PS_FLAG: MeasurementCountUnique__enable_count_unique_service
    SERVER_AUTO_GENERATED_FLAG_NAME: MeasurementCountUnique__aggregation_coordinator_origin
    
    Change-Id: I4856a0b9e35db08a3377687dcdeb4e60fce4d2c8

--- adservices/service-core/java/com/android/adservices/service/FlagsConstants.java
+++ adservices/service-core/java/com/android/adservices/service/FlagsConstants.java
@@ -1507,6 +1507,9 @@ public final class FlagsConstants {
     public static final String KEY_MEASUREMENT_COUNT_UNIQUE_MAX_CONTRIBUTION_LONG_WINDOW_MS =
             "MeasurementCountUnique__max_contribution_long_window_ms";
 
+    public static final String KEY_MEASUREMENT_COUNT_UNIQUE_AGGREGATION_COORDINATOR_ORIGIN =
+            "MeasurementCountUnique__aggregation_coordinator_origin";
+
     public static final String KEY_MEASUREMENT_COUNT_UNIQUE_REPORT_EXPIRY_MS =
             "MeasurementCountUnique__report_expiry_ms";
 
